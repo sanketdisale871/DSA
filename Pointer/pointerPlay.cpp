8e36c17cd281ece2b27440304e0f1f55146e8c38
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2022-10-10 19:40:16 +0530

    Update pointerPlay.cpp

--- Pointer/pointerPlay.cpp
+++ Pointer/pointerPlay.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+void update(int **p2){
+    
+    // p2 = p2 + 1;
+    // kucch chage hoga -> No
+    
+    // *p2 = *p2 + 1;
+    // kucch chage hoga -> YES
+    
+     **p2 = **p2 + 1;
+    // kucch chage hoga -> YES
+    
+    
+}
+
 
 int main(){
    int a = 10;
@@ -21,6 +35,19 @@ int main(){
    cout<<"\nPrinting Address of p1 :\n";
    cout<<&p1<<endl;
    cout<<p2<<endl;
+   
+   /*Double pointer as function */
+   
+   cout<<"\nBefore : "<<a<<endl;
+   cout<<"\nBefore : "<<p1<<endl;
+   cout<<"\nBefore : "<<p2<<endl;
+   
+   update(p2);
+   
+   cout<<"\nAfter : "<<a<<endl;
+   cout<<"\nAfter : "<<p1<<endl;
+   cout<<"\nAfter : "<<p2<<endl;
+
    
     
     return 0;
