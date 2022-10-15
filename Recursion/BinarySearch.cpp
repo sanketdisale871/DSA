e48bfdf5c936c89d0b08a52b5ff04166c4133ed3
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2022-10-15 09:19:08 +0530

    Added print function

--- Recursion/BinarySearch.cpp
+++ Recursion/BinarySearch.cpp
@@ -3,9 +3,18 @@
 
 using namespace std;
 
+void print(int *arr,int s,int e){
+    for(int i=s;i<=e;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+}
 
 
 bool BinarySearch(int *arr,int s,int e,int k){
+    
+   
+    print(arr,s,e);
     
     // Base Condition
     
@@ -14,6 +23,8 @@ bool BinarySearch(int *arr,int s,int e,int k){
     }
     
     int mid = s + (e-s)/2 ;
+    
+    cout<<"Value of mid is : "<<arr[mid]<<endl;
     
     if(arr[mid]==k){
         return true;
