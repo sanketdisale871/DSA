fcc9afe9fa25b32a2ad2b061451f9d62098badab
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2022-11-11 23:38:30 +0530

    Create 11_11PrimesSum.cpp
    link : https://practice.geeksforgeeks.org/problems/primes-sum5827/1

---
+++ gfgPTD/11_11PrimesSum.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+public:
+
+    bool isPrime(int n){
+        
+        if(n<2){
+            return false;
+        }
+        
+        for(int i=2;i*i<=n;i++){
+            
+            if(n%i==0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    string isSumOfTwo(int N){
+        
+        for(int i=2;i<N;i++){
+            
+            if(isPrime(i) && isPrime(N-i)){
+                return "Yes";
+            }
+        }
+        return "No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.isSumOfTwo(N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
