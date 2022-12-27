992389aa32f593696fb7242d8c8c95dc90d78ee2
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2022-12-27 12:06:03 +0530

    CheckArraySorted&Rotated
    https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/

--- Array/27_12CheckArraySorted.cpp
+++ Array/27_12CheckArraySorted.cpp
@@ -18,3 +18,19 @@ int main() {
   printf("%s", isSorted(arr, n) ? "True" : "False");
 
 }
+
+
+bool check(vector<int>& nums) {
+        /* hum jab previous element bada hoga agle se to and ye ek bar hi hona chahiye to o array sorted or rotated hai */
+    int n = nums.size();
+    int count =0;
+    for(int i=1;i<n;i++){  
+        if(nums[i-1]>nums[i]){ 
+            count++;
+        }
+    }
+    if(nums[n-1]>nums[0]){ /* jab last array element bada hoga pehlese to o array rotated nahi hai isliye count ko badha diya...*/
+        count++;
+    }
+   
+    return count<=1;  /* less than for equal elements in array */ 
