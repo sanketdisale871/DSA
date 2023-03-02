7058d1f19595d9c94d45c43ab2abec8c1a762517
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-03-02 22:28:31 +0530

    Update 2_3TrappingRain.cpp

--- Stacks/2_3TrappingRain.cpp
+++ Stacks/2_3TrappingRain.cpp
@@ -1,79 +1,81 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
+class Solution {
+public:
+/*
+    int trap(vector<int>& height) {
+        /* Approach1 : => left and right end there is no wall
+        => at any point we store water is :
+           water[i]= min (max_left_height,max_right_height)-arr[i];
+           
+           Time Complexity : O (3n) -> Traversing loop two times
+           Space Complexity : O(3n) -> Declaring two vector spaces
 
-using namespace std;
+            
+           int n = height.size();
+           vector<int>lmax(n,0);
+           vector<int>rmax(n,0);
 
+           int lmaxi = 0;
+           int rmaxi = 0;
 
-// } Driver Code Ends
-class Solution{
+           // contain left maximum element for each index element
 
-    // Approach 2 : Two Pointer Approach
-    /*
-    T.C. : O(n)
-    S.C. : O(1)
-    
+           for(int i=0;i<n;i++){
+               lmax[i]=lmaxi;
+               lmaxi = max(lmaxi,height[i]);
+           }
+          // contain right maximum element for each index element
+           for(int i=n-1;i>=0;i--){
+               rmax[i]=rmaxi;
+               rmaxi = max(rmaxi,height[i]);
+           } 
+
+           int cnt = 0;
+           for(int i=0;i<n;i++){
+               int res = min(lmax[i],rmax[i])-height[i];
+
+               if(res>0){
+                   cnt+=res;
+               }
+           }
+           return cnt;
+    }
     */
-    public:
-    long long trappingWater(int arr[], int n){
-        long long res = 0;
-        long long leftMax = 0;
-        long long rightMax = 0;
+
+    /* Approach 2 : Two Pointer approach
+            T.C = O(n)
+            S.C = O(1)  
+    */
+
+    int trap(vector<int>& height){
+        int n = height.size();
+        int lmax = 0;
+        int rmax = 0;
         int l = 0;
         int r = n-1;
+        int res = 0;
+
         while(l<r){
-            
-     if(arr[l]<=arr[r]){
-            if(arr[l]>=leftMax){
-                leftMax = arr[l];
+
+            if(height[l]<=height[r]){
+                if(height[l]>=lmax){
+                    lmax = height[l];
+                }
+                else{
+                    res+=(lmax - height[l]);
+                }
+                l++;
             }
             else{
-                res+=(leftMax - arr[l]);
+                if(height[r]>=rmax){
+                    rmax = height[r];
+                }
+                else{
+                    res+=(rmax-height[r]);
+                }
+                r--;
             }
-            l++;
-        }
-    else{
-        if(arr[r]>=rightMax){
-            rightMax = arr[r];
-        }
-        else{
-            res+=(rightMax - arr[r]);
-        }
-        r--;
-    }    
-            
         }
         return res;
-        
 
     }
 };
-
-//{ Driver Code Starts.
-
-int main(){
-    
-    int t;
-    //testcases
-    cin >> t;
-    
-    while(t--){
-        int n;
-        
-        //size of array
-        cin >> n;
-        
-        int a[n];
-        
-        //adding elements to the array
-        for(int i =0;i<n;i++){
-            cin >> a[i];            
-        }
-        Solution obj;
-        //calling trappingWater() function
-        cout << obj.trappingWater(a, n) << endl;
-        
-    }
-    
-    return 0;
-}
-// } Driver Code Ends
