2f048961285f32a47ddc6491efd0b3a5fcc86f71
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-10-27 07:07:32 +0530

    Time: 168 ms (40.69%) | Memory: 11.8 MB (54.70%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,33 +1,55 @@
 class Solution {
-    private:
-    bool isPalin(int i,int j,string &s){
-        while(i<j){
-            if(s[i]!=s[j]){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
 public:
     string longestPalindrome(string s) {
+        /*
+        Steps to Solve Problem : 
+        1) Cal for Palind substring of len 1
+        2) Cal for Palind substring of len 1
+        3) Check for other lengths        
+        */
         int n = s.length();
+        int dp[n][n];
 
-        int maxiLen  = 1;
+        int maxiLen = 1;
         int stPoint = 0;
 
+// One Length wala kam
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(isPalin(i,j,s)){
-                    if((j-i+1)>maxiLen){
-                        maxiLen = j-i+1;
+            for(int j=0;j<n;j++){
+                if(i==j){
+                    dp[i][j]=1;
+                }
+                else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+// Two Length wala kam
+        for(int i=1;i<n;i++){
+            if(s[i-1]==s[i]){
+                maxiLen = 2;
+                stPoint = i-1;
+                dp[i-1][i]=1;
+            }
+        }
+
+        // Three and above length wala 
+        for(int k=3;k<=n;k++){
+            for(int i=0;i<n-k+1;i++){
+                int j = i+k-1;
+
+                if(dp[i+1][j-1]==1 && s[i]==s[j]){
+                    dp[i][j]=1;
+                    if(k>maxiLen){
+                        maxiLen = k;
                         stPoint = i;
                     }
                 }
             }
         }
 
+
         return s.substr(stPoint,maxiLen);
+
     }
 };
