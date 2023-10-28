88c52b29ed14925d94e86052ff44be24cc4b17e2
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-10-28 13:31:40 +0530

    Time: 47 ms (74.23%) | Memory: 43.7 MB (61.25%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,63 +1,51 @@
 class Node{
     public:
-    Node* childrens[26];
+    Node* children[26];
     bool endOfWord = false;
 
-    // Check if the reference trie is present or not
     bool containsKey(char ch){
-        return (childrens[ch-'a']!=NULL);
+        return (children[ch-'a']!= NULL);
     }
 
-    // Creating regerence trie
-    void put(char ch,Node* node){
-        childrens[ch-'a']=node;
+    void put(char ch,Node *node){
+        children[ch-'a']=node;
     }
 
-    // To get the next node for traversal
     Node* get(char ch){
-        return childrens[ch-'a'];
+        return children[ch-'a'];
     }
 
-    // Setting flag true at the end of the word
     void setEnd(){
-       endOfWord = true;
+        endOfWord = true;
     }
 
-    // Checking if the word is completed or not
     bool isEnd(){
         return endOfWord;
     }
-
 };
 
 class Trie {
-private: Node* root;
-
+    private:
+    Node* root;
 public:
     Trie() {
-        // Creating new Object
         root = new Node();
     }
-
-    /// T.C : O(length of word)
+    
     void insert(string word) {
-        // Intializing dummy node poiting to root intially
         Node* node = root;
 
         for(int i=0;i<word.length();i++){
             if(!node->containsKey(word[i])){
                 node->put(word[i],new Node());
             }
-
-            // Moves to the referance trie !
             node = node->get(word[i]);
         }
         node->setEnd();
     }
     
-    /// T.C : O(length of word)
     bool search(string word) {
-        Node* node = root;
+        Node* node =root;
 
         for(int i=0;i<word.length();i++){
             if(!node->containsKey(word[i])){
@@ -65,11 +53,9 @@ public:
             }
             node = node->get(word[i]);
         }
-
-        return node->isEnd();        
+        return node->isEnd();
     }
-
-    /// T.C : O(length of word)
+    
     bool startsWith(string prefix) {
         Node* node = root;
 
