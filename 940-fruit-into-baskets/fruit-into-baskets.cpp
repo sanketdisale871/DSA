0e444750b3c77c986bb263488fca0c3ed0953843
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-10-29 13:21:45 +0530

    Time: 118 ms (80.12%) | Memory: 71.5 MB (59.58%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        unordered_map<int,int>um;
+        int i=0,j=0;
+        int maxi = 0;
+
+        while(j<n){
+            um[fruits[j]]++;
+
+            if(um.size()<=2){
+                maxi = max(maxi,(j-i+1));
+                j++;
+            }
+            else if(um.size()>2){
+                while(um.size()>2 && i<=j){
+                    um[fruits[i]]--;
+                    if(um[fruits[i]]==0){
+                        um.erase(fruits[i]);
+                    }
+                    i++;
+                }
+                j++;
+            }
+        }
+
+        return maxi;
+    }
+};
