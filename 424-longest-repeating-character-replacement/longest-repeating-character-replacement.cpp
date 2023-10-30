9809f77afccfb355b360bd1db72da1a52227e6ae
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-10-30 09:43:43 +0530

    Time: 11 ms (49.68%) | Memory: 7.4 MB (27.92%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char,int>um;
+
+        int n = s.length();
+        int maxiOccur = 0;
+        int maxiLen = 0;
+
+        int i=0,j=0;
+
+        while(j<n){
+            um[s[j]]++;
+
+            maxiOccur = max(maxiOccur,um[s[j]]);
+
+            if((j-i+1)-maxiOccur <=k){
+                maxiLen = max(maxiLen,j-i+1);
+                j++;
+            }
+            else{
+                um[s[i]]--;
+                i++;
+                j++;
+            }
+        }
+        return maxiLen;
+    }
+};
