7adb8653ffaf88aa490467939072b0f108c325e1
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-11-03 18:47:30 +0530

    Time: 4 ms (95.09%) | Memory: 11.2 MB (38.37%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>um;
+
+        for(int i=0;i<nums.size();i++){
+            if(um.find(target-nums[i])!=um.end()){
+                return {i,um[target-nums[i]]};
+            }
+
+            um[nums[i]]=i;
+        }
+        return {-1,-1};
+    }
+};
