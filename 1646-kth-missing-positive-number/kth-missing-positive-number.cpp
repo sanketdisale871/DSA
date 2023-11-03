ef227f61b42f024565cffb1a10826f0accd0f1c6
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-11-03 13:48:10 +0530

    Time: 3 ms (75.99%) | Memory: 10.2 MB (9.44%) - LeetSync

---
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+
+        for(int i=0;i<n;i++){
+            if(arr[i]<=k){
+                k++;
+            }
+            else{
+                break;
+            }
+        }
+        return k;
+    }
+};
