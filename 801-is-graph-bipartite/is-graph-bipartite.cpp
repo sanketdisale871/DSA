d88dd1a0ee4f3c269139381639d113cad30f1e3b
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-11-12 07:45:21 +0530

    Time: 19 ms (66.81%) | Memory: 13.9 MB (66.84%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    private:
+    bool dfsTrav(int col,int node,vector<int>&graphCol,vector<vector<int>>& graph){
+            graphCol[node]=col;
+
+            for(auto it : graph[node]){
+                if(graphCol[it]==-1){
+                    if(!dfsTrav(!col,it,graphCol,graph)){
+                        return false;
+                    }
+                }
+                else if(graphCol[it]==col){
+                    return false;
+                }
+            }
+            return true;
+    }
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+
+        vector<int>graphCol(n,-1);
+
+        for(int i=0;i<n;i++){
+            if(graphCol[i]==-1){
+                if(!dfsTrav(0,i,graphCol,graph)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
