428c34c01e494c02858b3084cb0bab1bff4e77d2
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-11-13 08:51:12 +0530

    Time: 20 ms (83.46%) | Memory: 26.2 MB (88.33%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> ans;
         int n = nums.size();
 
-        // Placing the tow pointers at correct position -> just after the positive and negative
-        int i,j;
+        vector<int>ans(n);
 
-        for(i=0;i<n;i++){
-            if(nums[i]>=0){
-                break;
-            }
-        }
-        j = i-1;
+        int i=0,j=n-1,k=n-1;
 
-        while((i<n) && (j>=0)){
-            if(nums[i]==abs(nums[j])){
-                ans.push_back(nums[i]*nums[i]);
-                ans.push_back(nums[i]*nums[i]);
-                i++;
-                j--;
-            }
-            else if(nums[i]<abs(nums[j])){
-                ans.push_back(nums[i]*nums[i]);
+        while(i<=j){
+            if(abs(nums[i])>=abs(nums[j])){
+                ans[k]=nums[i]*nums[i];
                 i++;
             }
             else{
-                ans.push_back(nums[j]*nums[j]);
+                ans[k]=nums[j]*nums[j];
                 j--;
             }
+            k--;
         }
-
-        while(i<n){
-            ans.push_back(nums[i]*nums[i]);
-            i++;
-        }
-        while(j>=0){
-            ans.push_back(nums[j]*nums[j]);
-            j--;
-        }
-
-
         return ans;
     }
 };
