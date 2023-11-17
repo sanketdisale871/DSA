791a750c2a7f9ac717b68cc232ce611edf4f5cb7
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-11-17 08:45:27 +0530

    Time: 250 ms (56.42%) | Memory: 122.9 MB (63.10%) - LeetSync

--- 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.cpp
+++ 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.cpp
@@ -4,20 +4,20 @@ typedef long long int ll;
 
     int val,lst;
     ll consCnt = 0;
+
     DataStream(int value, int k) {
-        val = value;
-        lst = k;
+        this->val = value;
+        this->lst = k;
     }
     
     bool consec(int num) {
-        cout<<val<<lst<<endl;
+ 
         if(num == val){
             consCnt++;
         }
         else{
             consCnt=0;
         }
-        cout<<"Num : "<<consCnt<<endl;
 
         if(consCnt>=lst){
             return true;
