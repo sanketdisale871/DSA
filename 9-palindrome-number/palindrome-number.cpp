59152b8e2c335b06bad59b3f7ef3b38d4abcd3c2
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-12-09 18:24:43 +0530

    Time: 16 ms (14.36%) | Memory: 15.9 MB (5.52%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        stack<int> st;
+        if(x<0)
+            return false;
+        int y=x;
+        while(x)
+        {
+            st.push(x%10);
+            x/=10;
+        }
+        while(y)
+        {
+            if(st.top()!=(y%10))
+               return false;
+             st.pop();
+               y/=10;
+        }
+        return true;
+    }
+};
