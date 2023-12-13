732e2a8a399fc4203ee924580f94f555959b3473
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-12-13 18:05:19 +0530

    Time: 207 ms (32.19%) | Memory: 131.4 MB (49.52%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        // <prefSum,k>
+        // map<pre,ind>um
         unordered_map<int,int>um;
 
-        int pr = 0;
+        um[0]=-1; // nums =[23,2,4,6,6]  k = 7 ==> [23,2,4,6,6]
+        //  pr[i] + k =>pr[j]
+        // [ x ,x ,x ,x ,x ,x]
+        // pr[j] = pr[i] + k ,, Take mod both side
+        // pr[j]%k = pr[i]%k + k%k
+        // pr[j]%k = pr[i]%k
+        // pr[i]%k = pr[j]%k
+
+        int preSum = 0;
 
         for(int i=0;i<nums.size();i++){
-            pr+=(nums[i]);
+            preSum+=nums[i];
 
-            if(pr%k == 0 && (i-0+1)>1){ // When any subarray from starting, e.g [23,2,4,6,6] and k = 7 ==> {23,2,4,6}
-                return true;
-            }
-
-            if(um.find(pr%k)!=um.end()){
-
-                if((i-um[pr%k])>1){
+            if(um.find(preSum%k)!=um.end()){
+                if(i-um[preSum%k] > 1){
                     return true;
                 }
             }
             else{
-                um[pr%k]=i;
+                um[preSum%k]=i;
             }
-
         }
         return false;
     }
