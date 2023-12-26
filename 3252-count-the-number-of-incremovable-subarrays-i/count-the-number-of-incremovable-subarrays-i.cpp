f9c64801ddc21654d1de9863c2ef4a4ef5e10cdb
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-12-26 14:09:17 +0530

    Time: 15 ms (85.88%) | Memory: 19.6 MB (56.03%) - LeetSync

--- 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.cpp
+++ 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.cpp
@@ -11,7 +11,6 @@ public:
                return false;
            }
        }
-
         // Right wala part array
         for(ll i=r+1;i<nums.size()-1;i++){
             if(nums[i]>=nums[i+1]){
@@ -19,8 +18,8 @@ public:
             }
         }
 
-        if(l>0 && r<n-1 && nums[l-1]>=nums[r+1]){
-            return false;
+        if(l>0 && r<n-1 && nums[l-1]>=nums[r+1]){ // Middle wali condition
+            return false; 
         }
 
         return true;
@@ -31,9 +30,38 @@ public:
 
         ll ans = 0;
 
+        // Precomputation
+        vector<ll>prefix(n,0);
+        vector<ll>suffix(n,0);
+        prefix[0]=1;
+        suffix[n-1]=1;
+
+        for(ll i=1;i<n;i++){
+            if(nums[i]>nums[i-1] && prefix[i-1]==1){
+                prefix[i]=1;
+            }            
+        }
+
+        for(ll i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1] && suffix[i+1]==1){
+                suffix[i]=1;
+            }
+        }
+
+
+
         for(ll i=0;i<n;i++){  
             for(ll j=i;j<n;j++){
-                if(check(i,j,nums)){
+
+                bool left = i==0 || prefix[i-1]==1 ? true:false;
+                bool right = j==n-1 || suffix[j+1]==1  ?true:false;
+                bool middle = true;
+
+                if(i>0 && j<n-1 && nums[i-1]>=nums[j+1]){ // Middle wali condition
+                    middle= false; 
+                }
+
+                if(left && right && middle){ // [0,....,i-1] [j+1,...n-1]
                     cout<<"i :  "<<i<<" j : "<<j<<endl;
                     ans++;
                 }
