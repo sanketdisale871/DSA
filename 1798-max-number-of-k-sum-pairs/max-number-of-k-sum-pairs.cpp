5e84e5f4857c69da1e1944139a5941a3ef631723
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-12-28 16:16:11 +0530

    Time: 535 ms (5.05%) | Memory: 105.1 MB (5.09%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
+// By using Two pointer approch, we can do in O(n)
     int maxOperations(vector<int>& nums, int k) {
         int cnt = 0;
         unordered_map<int,int>um;
-        unordered_map<int,int>vis;
+        // unordered_map<int,int>vis;
 
         for(auto num : nums){
             um[num]++;
@@ -13,17 +14,16 @@ public:
             int x = num;
             int y = k-num;
 
-            if(vis[x]==0 && vis[y]==0){
-                if(x==y){
-                    cnt+=(um[x]/2);
-                }
-                else{
-                    cnt+=min(um[x],um[y]);
-                }
+            if(x==y){
+                cnt+=(um[x]/2);
+            }
+            else{
+                cnt+=min(um[x],um[y]);
             }
 
-            vis[x]=1;
-            vis[y]=1;
+            um.erase(x);
+            um.erase(y);
+
         }
         return cnt;
     }
