b5837f090423af52fa18c2cc7522b8c30668d454
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-01-01 01:31:07 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (95.96%) - LeetSync

--- 2628-minimize-the-maximum-of-two-arrays/minimize-the-maximum-of-two-arrays.cpp
+++ 2628-minimize-the-maximum-of-two-arrays/minimize-the-maximum-of-two-arrays.cpp
@@ -1,43 +1,55 @@
 class Solution {
-public:
-    long gcd(long a, long b)
-    {
-        if((a%b)==0){
+    private:
+    typedef long long int ll;
+
+    ll gcd(ll a,ll b){
+        if(a%b == 0){
             return b;
-        }else{
-            return gcd(b, a%b);
         }
+
+        return gcd(b,a%b);
     }
+
+    ll lcm(ll d1,ll d2){
+        ll g = gcd(d1,d2);
+
+        cout<<"GCD : "<<g<<endl;
+
+        ll res = (d1/g)*d2;
+        cout<<"LCM : "<<res<<endl;
+        return res;
+    }
+
+    bool checkPossible(ll m,ll d1,ll d2,ll uc1,ll uc2,ll lcmm){
+        ll e1 =m-(m/d1);
+        ll e2=m-(m/d2);
+        ll c= m-(m/d1)-(m/d2)+(m/lcmm);
+
     
-    void bs(long l, long h, int divisor1, int divisor2, long lcm, long * ans, int c1, int c2){
-        if(l<=h){
-            long m=(l+h)/2;
-            
-            long a, b, c;
-            
-            a=m-(m/divisor1);
-            b=m-(m/divisor2);
-            c=m-(m/divisor1)-(m/divisor2)+(m/lcm);
-            
-            //cout<<m<<" "<<a<<" "<<b<<" "<<c<<"\n";
-            
-            if(a>=c1 && b>=c2 && a+b-c>=c1+c2){
-                (*ans)=m;
-                bs(l, m-1, divisor1, divisor2, lcm, ans, c1, c2);
-            }else{
-                bs(m+1, h, divisor1, divisor2, lcm, ans, c1, c2);
+
+        return e1>=uc1 && e2>=uc2 && (uc1+uc2 <= (e1+e2-c));
+    }
+
+public:
+    int minimizeSet(int d1, int d2, int uc1, int uc2) {
+        ll low = 0;
+        ll high = INT_MAX;
+
+        ll lcmm=lcm(d1,d2);
+
+        ll ans = 0;
+
+        while(low<=high){
+            ll mid = low + (high-low)/2;
+
+            if(checkPossible(mid,d1,d2,uc1,uc2,lcmm)){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
             }
         }
-    }
-    int minimizeSet(int divisor1, int divisor2, int uniqueCnt1, int uniqueCnt2) {
-        long g=gcd(max(divisor1, divisor2), min(divisor1, divisor2));
-        long l=(long(divisor1)*long(divisor2))/g;
-        
-        long ans=1000000000;
-        
-        bs(2, 10000000000, divisor1, divisor2, l, (&ans), uniqueCnt1, uniqueCnt2);
-        
-        return int(ans);
-        
+        return ans;
     }
 };
