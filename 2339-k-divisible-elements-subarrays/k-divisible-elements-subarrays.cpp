2e8ea38f519cc00759600569a11429bbcbd331d4
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-01-04 01:00:08 +0530

    Time: 237 ms (88.47%) | Memory: 109.7 MB (88.89%) - LeetSync

--- 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.cpp
+++ 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int countDistinct(vector<int>& nums, int k, int p) {
-        set<vector<int>>st;
-  
+        // set<vector<int>>st;
+        unordered_set<string>st;
+
         int n=nums.size();
         
         for(int i=0;i<n;i++){
-            vector<int>temp;
+            string temp;
             int cnt = 0;
 
             for(int j=i;j<n;j++){
                 
                 if(nums[j]%p==0){
                     cnt++;
-                }
-
+                } 
+                
                 temp.push_back(nums[j]);
 
                 if(cnt<=k){
