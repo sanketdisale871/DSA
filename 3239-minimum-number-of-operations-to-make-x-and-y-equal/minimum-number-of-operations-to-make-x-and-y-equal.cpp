667134b0c23ccef0afd7b104b0cc9e2fc7624cf3
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-01-06 22:58:24 +0530

    Time: 25 ms (15.79%) | Memory: 12.4 MB (15.79%) - LeetSync

--- 3239-minimum-number-of-operations-to-make-x-and-y-equal/minimum-number-of-operations-to-make-x-and-y-equal.cpp
+++ 3239-minimum-number-of-operations-to-make-x-and-y-equal/minimum-number-of-operations-to-make-x-and-y-equal.cpp
@@ -1,50 +1,36 @@
 class Solution {
 public:
-    int n = 1e4+100;
     int minimumOperationsToMakeEqual(int x, int y) {
+        if(x == y) return 0;
+        if(x < y) return y-x;
         
-
-        // I have to reach x----> y , SRC = x and DEST = y 
-
-        vector<int>dist(n,1e9);
-
-        // pair<int,int> == pair<cost,node>
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>minHeap;
-        dist[x]=0;
-        minHeap.push({0,x});
-
-        while(!minHeap.empty()){
-            auto it = minHeap.top();minHeap.pop();
-
-            int node = it.second;
-            int cost = it.first;
-
-            if(node%11 == 0 && dist[node]+1<dist[node/11]){
-                dist[node/11]=1+dist[node];
-                minHeap.push({dist[node/11],node/11});
+        int operations = 0;
+        queue<int> q;
+        q.push(x);
+        unordered_set<int> visited;
+        
+        while(!q.empty()) {
+            int len = q.size(); 
+            
+            for(int i = 0; i < len; i++) {
+                int a = q.front(); 
+                q.pop();
+                if(visited.find(a) != visited.end())
+                    continue;
+                visited.insert(a);
+                
+                if(a == y)
+                    return operations;
+                if(a%11 == 0)
+                    q.push(a/11);
+                if(a%5 == 0)
+                    q.push(a/5);
+                q.push(a+1);
+                q.push(a-1);
             }
-
-            if(node%5 == 0 && dist[node]+1<dist[node/5]){
-                dist[node/5]=1+dist[node];
-                minHeap.push({dist[node/5],node/5});
-            }
-
-            if(node>0 && dist[node]+1<dist[node-1]){
-                dist[node-1]=1+dist[node];
-                minHeap.push({dist[node-1],node-1});
-            }
-
-            if(node+1 <n && dist[node]+1<dist[node+1]){
-                dist[node+1]=dist[node]+1;
-                minHeap.push({dist[node+1],node+1});
-            }
+            operations++;
         }
-
-        for(int i=y;i<=x;i++){
-            cout<<dist[i]<<" ";
-        }
-        cout<<endl;
-
-        return dist[y];
+        
+        return -1;
     }
 };
