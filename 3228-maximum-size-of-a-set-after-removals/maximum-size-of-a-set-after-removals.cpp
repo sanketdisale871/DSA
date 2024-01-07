3ed9636413d97e524f685badece29944380dd2ab
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-01-07 14:08:30 +0530

    Time: 193 ms (33.33%) | Memory: 132.1 MB (11.11%) - LeetSync

--- 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.cpp
+++ 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
+// Explanation Link : https://leetcode.com/problems/maximum-size-of-a-set-after-removals/solutions/4520990/c-java-python-set-difference 
+
     int maximumSetSize(vector<int>& nums1, vector<int>& nums2) {
         unordered_set<int>st1(nums1.begin(),nums1.end());
         unordered_set<int>st2(nums2.begin(),nums2.end());
         int n = nums1.size();
-
         unordered_set<int>common;
 
         for(auto it:st1){
@@ -13,12 +14,11 @@ public:
             }
         }
 
-        int n1 = st1.size(); // Only element from Set A, will conribute in Final
-        int n2 = st2.size(); // Onely element from Set B, will contribute in Final
+        int n1 = st1.size(); // Only elements from Set A, will conribute in Final
+        int n2 = st2.size(); // Only elements from Set B, will contribute in Final
         int cmn = common.size();
 
-        int res = min(n/2,n1-cmn)+min(n/2,n2-cmn)+cmn;
-
+        int res = min(n/2,n1-cmn)+min(n/2,n2-cmn)+cmn; // Why i am taking min => At max from Array1 I can take n/2 elements 
         return res>n?n:res;
     }
 };
