6f02d5db44f47072cf30635fb2b524bcfd4e36b0
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-01-08 17:20:09 +0530

    Time: 56 ms (18.03%) | Memory: 61.1 MB (11.61%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    typedef long long int ll;
+    int countSubstrings(string s) {
+        ll n = s.length();
+        // Step1 : Marking a string Palindrome from [i.....j]
+
+        vector<vector<ll>>dp(n+1,vector<ll>(n+1)); // dp[i][j] -> Will tell me the is A palindrome from (i...j)
+
+        vector<vector<ll>>dp1(n+1,vector<ll>(n+1)); // dp[i][j] -> Wii tell me the number of Palindromes from (i..j)
+
+        // One length Always Palindrome 
+        for(ll i=1;i<=n;i++){
+            dp[i][i]=1;
+            dp1[i][i]=1;
+        }
+
+        // Two Length : aa,ab 
+        for(ll i=1;i<n;i++){
+            if(s[i-1]==s[i]){
+                dp[i][i+1]=1;
+            }
+            // Two Length 
+            dp1[i][i+1]=dp1[i][i]+dp1[i+1][i+1]+dp[i][i+1];
+        }
+
+        // 3 Length : aaa,aba,abc 
+        ll len = 3;
+        while(len<=n){
+           
+           ll i=1;
+           while(i<=(n-len+1)){
+               ll j = i+len-1;
+               if(s[i-1]==s[j-1] && dp[i+1][j-1]==1){
+                   dp[i][j]=1;
+               }
+               else{
+                   dp[i][j]=0;
+               }
+
+               dp1[i][j]=dp1[i][j-1]+dp1[i+1][j]-dp1[i+1][j-1]+dp[i][j];
+               i++;
+           }
+           len++;
+        }
+
+        return (int)dp1[1][n];
+    }
+};
