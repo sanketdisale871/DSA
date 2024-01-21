d4c80f04217690a3c593927a1b67b36cee13ea8c
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-01-21 17:12:22 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     typedef long long int ll;
-    ll dp[102][2];
+    ll dp[102];
     int rob(vector<int>& nums) {
         
         memset(dp,0,sizeof(dp));
 
         int n = nums.size();
 
-        dp[1][1]=nums[0];
-        dp[1][0]=0;
+        dp[1]=nums[0];
 
         for(int i=2;i<=n;i++){
-
-            dp[i][1]=nums[i-1]+max(dp[i-2][1],dp[i-1][0]);
-
-            dp[i][0]=max(dp[i-1][0],dp[i-1][1]);
+            dp[i]=max(nums[i-1]+dp[i-2],dp[i-1]);
         }
-        return max(dp[n][1],dp[n][0]);
+        return dp[n];
     }
 };
