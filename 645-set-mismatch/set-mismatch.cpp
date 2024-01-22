549f8924b43e01f75f046d8794a440919187beab
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-01-22 20:27:13 +0530

    Time: 11 ms (99.73%) | Memory: 23.8 MB (35.68%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
+    typedef long long int ll;
     vector<int> findErrorNums(vector<int>& nums) {
-        int dupli = -1;
-        int misi = -1;
-
-        unordered_map<int,int>um;
-
+        ll n = nums.size();
+        // x => Missing, y = duplicate
+        // Sn-S = Repeating , x--y = val1 
+        ll sum = 0;
+        ll sum1 = 0;
         for(auto num:nums){
-            um[num]++;
+            sum+=num;
+            sum1+=(num*1ll*num);
         }
 
-        for(int i=1;i<=nums.size();i++){
-            if(um[i]==0){
-                misi = i;
-            }
-            else if(um[i]>1){
-                dupli = i;
-            }
-        }
+        ll sn = n*1ll*(n+1)/2;
+        ll val1 = sn-sum; // x-y = val1 
 
-        return {dupli,misi};
+        ll sn2 = n*(n+1)*(2*n+1)/6;
+
+        ll val2 = sn2-sum1;
+
+        ll val3 = val2/val1;
+
+        ll x = (val1+val3)/2;
+        ll y = val3-x;
+
+        return {(int)y,(int)x};
+
+        
+
     }
 };
