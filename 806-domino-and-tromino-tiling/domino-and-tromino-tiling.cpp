de4a431ee1c3dc9339263c410c4c969b8e988731
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-01-25 01:08:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (6.62%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -3,30 +3,29 @@ public:
     typedef long long int ll;
     const int mod = 1e9+7;
     int numTilings(int n) {
-        // dp[i] => Answer of Question, if Size of array is `i`. Horizontal + Vertical + UpperTrim + LowerTriam 
+        vector<ll>dp(n+2,0);
+        vector<ll>upDp(n+2,0);
+        vector<ll>dwDp(n+2,0);
 
-        vector<ll>dp(1005);
-        vector<ll>dpUp(1005); // For trimino
-        vector<ll>dpDw(1005); // For trimino 
+        dp[1]=1;
+        dwDp[1]=0;
+        upDp[1]=0;
 
-        dpUp[1]=0;
-        dpDw[1]=0;
-        dp[1] = 1;
+        dp[2]=2;
+        dwDp[2]=1;
+        upDp[2]=1;
 
-        dpUp[2] = 1 ;
-        dpDw[2] = 1 ;
-        dp[2] = 2 ;
 
         for(ll i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dpDw[i-1]+dpUp[i-1];
+            dp[i]=dp[i-1]+dp[i-2]+upDp[i-1]+dwDp[i-1];
             dp[i]%=mod;
 
-            dpUp[i]=dpDw[i-1]+dp[i-2];
-            dpUp[i]%=mod;
-            dpDw[i]=dpUp[i-1]+dp[i-2];
-            dpDw[i]%=mod;
+            upDp[i]=dp[i-2]+dwDp[i-1];
+            upDp[i]%=mod;
+            dwDp[i]=dp[i-2]+upDp[i-1];
+            dwDp[i]%=mod;
         }
 
-        return dp[n];
+        return dp[n]; // The no.of all ways to place domino, tromino 
     }
 };
