f16708943ce3a70186ec9d0931f75a1edccdc865
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-01-25 14:51:53 +0530

    Time: 150 ms (98.37%) | Memory: 124.7 MB (58.91%) - LeetSync

---
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    typedef int ll;
+    int constrainedSubsetSum(vector<int>& nums, int k) {
+        deque<ll>dq;
+
+        vector<ll>cpy = nums;
+        ll maxi =INT_MIN;
+
+        ll n = nums.size();
+
+        for(ll i=0;i<n;i++){
+            ll fntElem = dq.empty()?0:cpy[dq.front()];
+
+            cpy[i]=max(cpy[i],cpy[i]+fntElem);
+
+            maxi = max(maxi,cpy[i]);
+
+            while(!dq.empty() && cpy[dq.back()]<cpy[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+
+            if(i>=k && cpy[dq.front()]==cpy[i-k]){
+                dq.pop_front();
+            }
+        }
+        return maxi;
+
+    }
+};
