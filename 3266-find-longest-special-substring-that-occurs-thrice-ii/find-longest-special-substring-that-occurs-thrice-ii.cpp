2eb93abbee3960d402c87ef937b1b073e36b55d1
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-01-25 11:34:40 +0530

    Time: 2888 ms (5.00%) | Memory: 144.4 MB (17.57%) - LeetSync

--- 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.cpp
+++ 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
-    private:
+public:
     typedef long long int ll;
-    bool isPossible(string &s,int len){
-        ll i=0,j=0,n=s.length();
 
+    bool isPossible(ll mid,string s){
         unordered_map<char,int>um;
-        vector<int>occr(26,0);
-
+     
+        vector<ll>occr(26,0);
+        ll i=0,j=0,n = s.length();
+        
         while(j<n){
             um[s[j]]++;
 
-            if(j-i+1 < len){
+            if((j-i+1)<mid){
                 j++;
             }
             else{
-
                 if(um.size()==1){
-                    occr[s[j]-'a']++;
+                   occr[s[j]-'a']++;
 
-                    if(occr[s[j]-'a']>2){
-                        return true;
-                    }
+                   if(occr[s[j]-'a']>=3){
+                       return true;
+                   }
                 }
-
+                // Sliding Step 
                 um[s[i]]--;
 
                 if(um[s[i]]==0){
@@ -33,22 +33,20 @@ class Solution {
                 j++;
             }
         }
-
         return false;
     }
-public:
+
     int maximumLength(string s) {
+        
         ll n = s.length();
-
-        ll low = 1;
-        ll high=n-2;
-
+        ll low = 0;
+        ll high = n;
         ll ans = -1;
 
         while(low<=high){
-            ll mid = (low+high)/2;
+            ll mid = (low + high)/2;
 
-            if(isPossible(s,mid)){
+            if(isPossible(mid,s)){
                 ans = mid;
                 low = mid+1;
             }
