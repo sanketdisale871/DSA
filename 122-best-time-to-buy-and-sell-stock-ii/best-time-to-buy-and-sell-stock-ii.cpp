684030762a0a774add6d1800efaafee743254d23
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-01-26 17:04:23 +0530

    Time: 10 ms (25.93%) | Memory: 20.5 MB (5.67%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    private:
+    typedef long long int ll;
+
+    vector<vector<ll>>dp;
+    ll findPr(ll ind,ll n,vector<int>&pr,ll buy){
+        // Base case 
+        if(ind>=n){
+            return 0;
+        }
+
+        if(dp[ind][buy]!=-1){
+            return dp[ind][buy];
+        }
+
+        if(buy){
+            ll opt1 = -pr[ind]+findPr(ind+1,n,pr,0);
+            ll opt2 = findPr(ind+1,n,pr,1);
+
+            return  dp[ind][buy]=max(opt1,opt2);
+        }
+        else{
+            ll opt1 = pr[ind]+findPr(ind+1,n,pr,1);
+            ll opt2 = findPr(ind+1,n,pr,0);
+
+            return  dp[ind][buy]=max(opt1,opt2);
+        }
+    }
+
+public:
+    int maxProfit(vector<int>& prices) {
+        ll n  = prices.size();
+
+        dp.assign(n+1,vector<ll>(3,-1));
+
+        ll b = 1;
+        return findPr(0,prices.size(),prices,b);
+    }
+};
