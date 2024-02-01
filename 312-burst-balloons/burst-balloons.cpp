f5ae8e440ca25257349f0edea16e1a7d09f9a34a
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-02-01 11:43:38 +0530

    Time: 134 ms (90.87%) | Memory: 14.2 MB (8.16%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,53 +1,53 @@
 class Solution {
 public:
     typedef long long int ll;
-    int maxCoins(vector<int>& arr) {
-        ll n = arr.size();
+    int maxCoins(vector<int>& nums) {
+        ll n = nums.size();
 
-        vector<int>nums;
-        nums.push_back(1);
-        // For avoiding Runtime Errors, making given arrays as 1 based indexing 
-        for(auto it:arr){
-            nums.push_back(it);
+        vector<ll>arr;
+        arr.push_back(1);
+        for(auto it:nums){
+            arr.push_back(it);
         }
-        nums.push_back(1);
+        arr.push_back(1);
 
-        vector<vector<ll>>dp(n+1,vector<ll>(n+1));
-        // dp[i][j] => Best answer from [i.........j]
+        // dp[i][j] => Maxi coints i get by removing the coins from [i.....j]
+        vector<vector<ll>>dp(n+5,vector<ll>(n+5,0)); 
 
-        // Length = 1 
         for(ll i=1;i<=n;i++){
-            dp[i][i]=nums[i-1]*nums[i]*nums[i+1];
+            dp[i][i]=arr[i-1]*arr[i]*arr[i+1];
         }
 
-        // Length 2 and above 
-        ll len = 2;
+        ll len=2;
         while(len<=n){
             ll i=1;
             while(i<=(n-len+1)){
-                ll j = i+len-1;
-                ll maxi = INT_MIN;
-                ll k = i; // k -> Burst this balloon at last 
+                ll j  =i+len-1;
+
+                ll res = INT_MIN;
+
+                ll k=i;
                 while(k<=j){
 
-                    ll res = nums[i-1]*nums[k]*nums[j+1];
+                    ll temp = arr[i-1]*arr[k]*arr[j+1];
 
                     if(i!=k){
-                        res+=dp[i][k-1] ;
+                        temp+=dp[i][k-1];
                     }
 
                     if(j!=k){
-                        res+=dp[k+1][j];
+                        temp+=dp[k+1][j];
                     }
 
-                    maxi = max(maxi,res);
+                    res = max(res,temp);
                     k++;
                 }
-                dp[i][j]=maxi; 
-                i++;               
+                dp[i][j]=res;
+                i++;
             }
             len++;
         }
         return dp[1][n];
+
     }
 };
