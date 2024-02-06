630b8d63344f4ea0a410a64421163caaf25e43d7
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-02-06 20:23:22 +0530

    Time: 3 ms (20.71%) | Memory: 7.1 MB (17.15%) - LeetSync

--- 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -3,6 +3,8 @@ public:
     int minOperations(int n) {
         int addOper = 0;
 
+        // 1110111 => By Adding 1 to 0Th bit position => 1111000 , After adding 1 to the 4Th Position, => 100000000  , Now after substracting => 0.   
+
         for(int i=0;i<20;i++){
             if(__builtin_popcount(n+(1<<i)) < __builtin_popcount(n)){
                 addOper++;
@@ -11,7 +13,6 @@ public:
         }
 
         int subOp = __builtin_popcount(n);
-
         return subOp+addOper;
     }
 };
