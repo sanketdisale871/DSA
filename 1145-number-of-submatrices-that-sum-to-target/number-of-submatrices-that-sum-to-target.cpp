7ea6d736adc851abcc5793c73d12e26412a73ffc
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-02-07 13:39:51 +0530

    Time: 660 ms (24.36%) | Memory: 257.5 MB (7.38%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -1,67 +1,39 @@
 class Solution {
 public:
-typedef long long int ll;
     int numSubmatrixSumTarget(vector<vector<int>>& mat, int target) {
-        ll m = mat.size();
-        ll n = mat[0].size();
-        ll ans = 0;
+        int m = mat.size();
+        int n = mat[0].size();
+        int ans = 0;
 
-        vector<vector<ll>>pref(m,vector<ll>(n,0));
+        vector<vector<int>>pref(m,vector<int>(n,0));
 
-        for(ll i=0;i<m;i++){
-            pref[i][0]=mat[i][0];
-            
-            for(ll j=1;j<n;j++){
-                pref[i][j]=pref[i][j-1]+mat[i][j];
-                // cout<<"pref[i][j] : "<<pref[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-
-        // for(ll colStart =0;colStart<n;colStart++){
-        //     for(ll colEnd=colStart;colEnd<n;colEnd++){
-        //         for(ll stRow=0;stRow<m;stRow++){
-        //             ll sum = 0;
-        //             for(ll endRow=stRow;endRow<m;endRow++){
-        //                  sum+=pref[endRow][colEnd]-(colStart?pref[endRow][colStart-1]:0);
-
-        //                  if(sum==target){
-        //                      ans++;
-        //                  }
-        //             }
-        //         }
-        //     }
-        // }
-
-            for(ll stCol=0;stCol<n;stCol++){
-                for(ll endCol=stCol;endCol<n;endCol++){
-                    unordered_map<ll,ll>um;
-                    um[0]=1;
-                    ll sum = 0;
-
-                    for(ll r=0;r<m;r++){
-                        sum+=pref[r][endCol]-(stCol > 0 ? pref[r][stCol-1]:0);
-
-                        ans+=um[sum-target];
-
-                        um[sum]++;
-                    }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(j==0){
+                    pref[i][j]=mat[i][j];
+                }
+                else{
+                    pref[i][j]=mat[i][j]+pref[i][j-1];
                 }
             }
+        }
 
-        // unordered_map<int, int> counter;
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = i; j < n; j++) {
-        //         counter = {{0,1}};
-        //         int cur = 0;
-        //         for (int k = 0; k < m; k++) {
-        //             cur += pref[k][j] - (i > 0 ? pref[k][i - 1] : 0);
-        //             ans += counter.find(cur - target) != counter.end() ? counter[cur - target] : 0;
-        //             counter[cur]++;
-        //         }
-        //     }
-        // }
 
+        for(int stCol=0;stCol<n;stCol++){
+            for(int endCol=stCol;endCol<n;endCol++){
+                int sum = 0;
+                unordered_map<int,int>um;
+                um[0]=1;
+
+                for(int r=0;r<m;r++){
+                    sum+=pref[r][endCol]-(stCol?pref[r][stCol-1]:0);
+
+                    ans+=um[sum-target];
+
+                    um[sum]++;
+                }
+            }
+        }
         return ans;
     }
 };
