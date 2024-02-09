060a9796afae824bd7846ebb2991abad86c7b522
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-02-09 16:56:09 +0530

    Time: 17 ms (76.62%) | Memory: 26 MB (98.79%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -11,28 +11,44 @@
  */
 class Solution {
     private:
-    int findMaxiPath(TreeNode* root,int &maxi){
+    // First time : Comes in my Mind 
+    // int findMaxiPath(TreeNode* root,int &maxi){
+    //     // Base case
+    //     if(root==NULL){
+    //         return 0;
+    //     }
+
+    //     int lefPa = findMaxiPath(root->left,maxi);
+    //     int rigPa = findMaxiPath(root->right,maxi);
+
+    //     int res = lefPa + rigPa + root->val;
+    //     maxi = max(maxi,res); // By considering node + leftPath  + rightPath
+    //     maxi = max(maxi,root->val); //  By considering only node
+    //     maxi = max(maxi,root->val+lefPa);//  By considering node + leftPath
+    //     maxi = max(maxi,root->val+rigPa); // By Considering node + rightPath
+
+    //     return max(root->val+max(lefPa,rigPa),root->val); // Returning a singleNode, aur left/right maximum wala node from currentNode se..
+    // }
+
+    int findMaxiPath1(TreeNode*root,int &maxi){
         // Base case
         if(root==NULL){
             return 0;
         }
 
-        int lefPa = findMaxiPath(root->left,maxi);
-        int rigPa = findMaxiPath(root->right,maxi);
+        int lefPath = max(0,findMaxiPath1(root->left,maxi)); // Hamesha negative wali aur negative bana degi
+        int riPath = max(0,findMaxiPath1(root->right,maxi));
 
-        int res = lefPa + rigPa + root->val;
+        int res = root->val + lefPath + riPath;
         maxi = max(maxi,res);
-        maxi = max(maxi,root->val);
-        maxi = max(maxi,root->val+lefPa);
-        maxi = max(maxi,root->val+rigPa);
 
-        return max(root->val+max(lefPa,rigPa),root->val);
+        return root->val+max(lefPath,riPath);
     }
 public:
     int maxPathSum(TreeNode* root) {
         int maxi = INT_MIN;
 
-        findMaxiPath(root,maxi);
+        findMaxiPath1(root,maxi);
 
         return maxi;
     }
