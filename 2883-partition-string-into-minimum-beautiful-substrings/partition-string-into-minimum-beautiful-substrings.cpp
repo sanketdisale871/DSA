35be7ba3516a2bd137a31f3f442bf9a5ad85460d
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-02-13 22:46:20 +0530

    Time: 4 ms (52.35%) | Memory: 9.8 MB (21.55%) - LeetSync

--- 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
+++ 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
     typedef long long int ll;
-    
     int minimumBeautifulSubstrings(string s) {
-        ll dp[500]={1000};
-        
-        ll n = s.length();
-        
-        // 0-length ke liye
+        vector<ll>dp(17,1e8);
+
+        // dp[i] => Tell the Minimum number of substring from [1,....,i]
+
+        int n = s.length();
         dp[0]=0;
-        
-        // 1-length ke liye 
-        if(s[0]=='0'){
-            dp[1]=1000;
-            
-            if(s.length()==1){
-                return -1;
-            }
-        }
-        else{
+
+        if(s[0]=='1'){
             dp[1]=1;
         }
-        
-        for(ll j=2;j<=n;j++){
-            ll i = j;
-            ll res = 1e8;
-            
-            while(i>=1){
-                string temp = s.substr(i-1,j-i+1);
-                
-                unsigned long dece = stoi(temp,0,2);
-                
-                if(temp[0]!='0' && (15625 % dece) == 0){
-                    res = min(res,dp[i-1]+1);
-                }
-                i--;
-            }
-            dp[j]=res;
+        else{
+            dp[1]=1e8;
+
         }
-        
+
+        for(int i=1;i<n;i++){
+            int j = i;
+
+            while(j>=0){
+                string temp = s.substr(j,i-j+1);
+
+                unsigned long dece = stoi(temp,0,2);
+
+                if(temp[0]!='0' && 15625%dece==0){
+                    dp[i+1]=min(dp[i+1],1+dp[j]);
+                }
+                j--;
+            }
+        }
+
         if(dp[n]==1e8){
             return -1;
         }
-        else{
-            return (int)dp[n];
-        }        
+
+        return dp[n];
     }
 };
