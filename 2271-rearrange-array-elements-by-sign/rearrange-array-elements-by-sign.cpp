4fe3d29a6fe1da574aaec1183cba3ae7e1d8ece5
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-02-14 22:15:30 +0530

    Time: 162 ms (41.85%) | Memory: 126.4 MB (42.43%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -4,31 +4,19 @@ public:
         int n = nums.size();
         vector<int>ans(n);
 
-        queue<int>qPos;
-        queue<int>qNeg;
+        int posInd = 0;
+        int negInd = 1;
 
         for(auto it:nums){
             if(it<0){
-                qNeg.push(it);
+                ans[negInd]=it;
+                negInd+=2;
             }
             else{
-                qPos.push(it);
+                ans[posInd]=it;
+                posInd+=2;
             }
         }
-
-        int itr = 0;
-
-        while(!qPos.empty() && !qNeg.empty()){
-            int firNum = qPos.front();qPos.pop();
-            int secNum = qNeg.front();qNeg.pop();
-
-            ans[itr++]=firNum;
-            ans[itr++]=secNum;
-        }
-
-
-
-
 
         return ans;
     }
