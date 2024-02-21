13a85ae9c962a652228d13220ee5f39942cf3769
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-02-21 19:13:14 +0530

    Time: 143 ms (56.71%) | Memory: 114.3 MB (30.38%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.cpp
+++ 2725-mice-and-cheese/mice-and-cheese.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    int miceAndCheese(vector<int>& r1, vector<int>& r2, int k) {
-        // long long int sum = 0;
-        // // pair<diff,amnt>
-        // priority_queue<pair<int,int>>maxHeap;
+    int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
+        long long int sum = 0;
+        // pair<diff,amnt>
+        priority_queue<pair<int,int>>maxHeap;
 
-        // int n = reward1.size();
+        int n = reward1.size();
 
-        // for(int i=0;i<n;i++){
-        //     maxHeap.push({reward1[i]-reward2[i],i});
-        // }
-
-        // while(k--){
-        //     auto it=maxHeap.top();maxHeap.pop();
-        //     int ind = it.second;
-        //     sum+=(reward1[ind]);
-        //     reward2[ind]=0;
-        // }
-
-        // for(int i=0;i<n;i++){
-        //     sum+=(reward2[i]);
-        // } 
-        // return (int)sum;
-
-        int n=r1.size();
-        vector<pair<int,int>> dif(n);
-        for (int i=0;i<n;i++)dif[i]={r1[i]-r2[i],i};
-        sort(dif.rbegin(),dif.rend());
-        int ans=0;
-        for (int i=0;i<k;i++){
-            ans+=r1[dif[i].second];
-            r2[dif[i].second]=0;
+        for(int i=0;i<n;i++){
+            maxHeap.push({reward1[i]-reward2[i],i});
         }
-        for (int i:r2)ans+=i;
-        return ans;
+
+        while(k--){
+            auto it=maxHeap.top();maxHeap.pop();
+            int ind = it.second;
+            sum+=(reward1[ind]);
+            reward2[ind]=0;
+        }
+
+        for(int i=0;i<n;i++){
+            sum+=(reward2[i]);
+        } 
+        return (int)sum;
+
+        
         
     }
 };
