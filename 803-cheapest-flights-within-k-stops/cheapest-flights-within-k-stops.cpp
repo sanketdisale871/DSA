3aff1b2e1a3ec5197d83fdb12757ba49a9b1159c
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-02-23 12:46:29 +0530

    Time: 18 ms (56.74%) | Memory: 17.2 MB (23.47%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,51 +1,54 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-    vector<pair<int,int>>adj[n];
 
-    for(auto it:flights){
-        int u = it[0];
-        int v = it[1];
-        int wt = it[2];
+        vector<pair<int,int>>adj[n];
 
-        adj[u].push_back({v,wt});
-    }
-
-    vector<int>dist(n,INT_MAX);
-    dist[src]=0;
-
-    // pair<stops,pair<price,node>>
-    priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>minHeap;
-
-    minHeap.push({0,{0,src}});
-
-    while(!minHeap.empty()){
-        auto it = minHeap.top();minHeap.pop();
-
-        int st = it.first;
-        int pr = it.second.first;
-        int node = it.second.second;
-
-        if(st>k){
-            break;
+        for(auto it:flights){
+            adj[it[0]].push_back({it[1],it[2]});
         }
 
-        for(auto it:adj[node]){
-            int adjNode = it.first;
-            int adjCost = it.second;
+        vector<int>dist(n+1,INT_MAX);
+        
+        // pair<int,pair<int,int>> pair<stops,pair<cost,node>>
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>minHeap;
 
-            if(pr+adjCost<dist[adjNode]){
-                dist[adjNode]=pr+adjCost;
-                minHeap.push({st+1,{dist[adjNode],adjNode}});
+        minHeap.push({0,{0,src}});
+
+        dist[src]=0;
+
+        while(!minHeap.empty()){
+
+            auto it = minHeap.top();minHeap.pop();
+            int stps = it.first;
+            int cst = it.second.first;
+            int node = it.second.second;
+
+            if(stps>k){
+                break;
             }
+
+
+            for(auto itt:adj[node]){
+
+                int adjNode = itt.first;
+                int adjCost = itt.second;
+
+                if(cst+adjCost<dist[adjNode]){
+                    dist[adjNode]=cst+adjCost;
+                    minHeap.push({stps+1,{dist[adjNode],adjNode}});
+                }
+
+            }
+
+
         }
-    }
 
-    if(dist[dst]==INT_MAX){
-        return -1;
-    }
+        if(dist[dst]==INT_MAX){
+            return -1;
+        }
 
-    return dist[dst];
+        return dist[dst];
 
     }
 };
