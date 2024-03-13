eb7d60f08d1089d1e43cebace11c31d8274bda66
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-03-13 12:39:36 +0530

    Time: 3 ms (51.39%) | Memory: 7.1 MB (73.82%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int leftSum = 0;
-        int rightSum = n*(n+1)/2;
+        int totSum = n*(n+1)/2;
 
-        for(int num=1;num<=n;num++){
-            leftSum+=num;
-            if(leftSum==rightSum){
-                return num;
-            }
-            rightSum-=num;
-            // cout<<"Num : "<<num<<endl;
-            // cout<<"LeftSum : "<<leftSum<<endl;
-            // cout<<"rightSum : "<<rightSum<<endl;
 
-            if(leftSum>rightSum){
-                return -1;
-            }
+        int sqrtt = sqrt(totSum);
+
+        if(sqrtt*1ll*sqrtt==totSum){
+            return sqrtt;
         }
         return -1;
     }
