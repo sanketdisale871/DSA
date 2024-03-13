224916b7f2c3f700dc41e300742e757f29c38b1d
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-03-13 12:32:55 +0530

    Time: 3 ms (51.39%) | Memory: 7.1 MB (73.82%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int leftSum = 0;
+        int rightSum = n*(n+1)/2;
+
+        for(int num=1;num<=n;num++){
+            leftSum+=num;
+            if(leftSum==rightSum){
+                return num;
+            }
+            rightSum-=num;
+            // cout<<"Num : "<<num<<endl;
+            // cout<<"LeftSum : "<<leftSum<<endl;
+            // cout<<"rightSum : "<<rightSum<<endl;
+
+
+
+        }
+        return -1;
+    }
+};
