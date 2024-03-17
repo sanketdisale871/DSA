8a6bc72ecf3022dc4f75a6c91472e0cdcebef4c0
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-03-17 19:21:55 +0530

    Time: 7 ms (83.33%) | Memory: 20.9 MB (100.00%) - LeetSync

--- 3345-find-the-sum-of-the-power-of-all-subsequences/find-the-sum-of-the-power-of-all-subsequences.cpp
+++ 3345-find-the-sum-of-the-power-of-all-subsequences/find-the-sum-of-the-power-of-all-subsequences.cpp
@@ -3,7 +3,7 @@ public:
     typedef long long int ll;
     const int mod = 1e9+7;
 
-    ll dp[101][101][101];
+    ll dp[101][101];
 
     ll power(ll x, ll y)
     {
@@ -17,10 +17,10 @@ public:
             return (x * temp * temp)%mod;
     }
 
-    ll findSub(ll ind,ll len,ll n,ll sum,ll k,vector<int>&nums){
+    ll findSub(ll ind,ll n,ll sum,ll k,vector<int>&nums){
         // base case
         if(sum==k){
-            ll t = (n-len)%mod;
+            ll t = (n-ind)%mod; // leftChars
             ll p = 2;
             return power(p,t)%mod;
         }
@@ -29,26 +29,26 @@ public:
             return 0;
         }
 
-        if(dp[ind][len][sum]!=-1){
-            return dp[ind][len][sum];
+        if(dp[ind][sum]!=-1){
+            return dp[ind][sum];
         }
 
-        ll tk = findSub(ind+1,len+1,n,sum+nums[ind],k,nums)%mod;
-        ll ntk = findSub(ind+1,len,n,sum,k,nums)%mod;
+        ll tk = findSub(ind+1,n,sum+nums[ind],k,nums)%mod;
+        ll ntk = 2*findSub(ind+1,n,sum,k,nums)%mod;
 
 
-        return dp[ind][len][sum]=(tk+ntk)%mod;
+        return dp[ind][sum]=(tk+ntk)%mod;
     }
 
     int sumOfPower(vector<int>& nums, int k) {
         
         for(ll i=0;i<101;i++){
-            for(ll j=0;j<101;j++){
+          
                 for(ll k=0;k<101;k++){
-                    dp[i][j][k]=-1;
+                    dp[i][k]=-1;
                 }
-            }
+            
         }
-        return findSub(0,0,nums.size(),0,k,nums);
+        return findSub(0,nums.size(),0,k,nums);
     }
 };
