6908d847735cdaf9f7df92d72c9ec14c93290e39
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-03-18 18:25:17 +0530

    Time: 1084 ms (9.23%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
+-- select email as Email
+-- from Person
+-- group by email
+-- having count(email)>1
 
-select distinct P1.email
-from Person P1,Person P2
-where P1.id <> P2.id and P1.email = P2.email;
+# Same mail id But Different Id wrt?
+
+select distinct P1.email as Email
+from Person as P1,
+Person as P2
+where P1.email = P2.email and P1.id != p2.id;
