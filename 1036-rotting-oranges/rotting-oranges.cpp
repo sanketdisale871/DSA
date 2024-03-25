cc838378da5ed4509fcf73f7c240c4529644b89a
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-03-25 14:32:16 +0530

    Time: 2 ms (85.35%) | Memory: 15.8 MB (82.84%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,59 +3,51 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int time = 0;
 
+        // pair<t,pair<r,c>>
         queue<pair<int,pair<int,int>>>q;
-        vector<vector<int>>vis(m,vector<int>(n,0));
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    vis[i][j]=1;
                     q.push({0,{i,j}});
                 }
             }
         }
 
+        int ans = 0;
 
         while(!q.empty()){
-            int siz = q.size();
+            auto it = q.front();q.pop();
 
-            for(int i=0;i<siz;i++){
-                auto p =q.front(); q.pop();
+            ans = max(ans,it.first);
+            int x = it.second.first;
+            int y = it.second.second;
 
-                int currTime = p.first;
-                int iNode = p.second.first;
-                int jNode = p.second.second;
+            int drow[] = {-1,1,0,0};
+            int dcol[] = {0,0,-1,1};
 
-                time = max(time,currTime);
+            for(int i=0;i<4;i++){
+                int newX = x+drow[i];
+                int newY = y+dcol[i];
 
-                int drow[]={1,-1,0,0};
-                int dcol[]={0,0,1,-1};
-
-                for(int i=0;i<4;i++){
-                    int newX = drow[i]+iNode;
-                    int newY = dcol[i]+jNode;
-
-                    if((newX>=0 && newX<m) && (newY>=0 && newY<n) && (!vis[newX][newY]) && (grid[newX][newY]==1)){
-                        vis[newX][newY]=1;
-                        q.push({currTime+1,{newX,newY}});
-                    }
+                if((newX>=0 && newX<m) && (newY>=0 && newY<n) && 
+                grid[newX][newY]==1){
+                    grid[newX][newY]=-1;
+                    q.push({it.first+1,{newX,newY}});
                 }
-
             }
         }
 
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==1 && vis[i][j]==0){
+            for(int j=0;j<n;j++){
+            
+                if(grid[i][j]==1){
                     return -1;
                 }
             }
         }
 
-
-        return time;
+        return ans;
     }
 };
