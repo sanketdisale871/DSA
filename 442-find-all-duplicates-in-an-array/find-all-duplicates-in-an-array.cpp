20c2da30e98cac565771eea890fca1e5fee6ad7f
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-03-25 07:32:27 +0530

    Time: 30 ms (95.98%) | Memory: 36 MB (68.64%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,18 +1,35 @@
 class Solution {
+
+/*
+Think in terms of on and off. Turning on and turning off a switch. If a switch is already turned on, you can not turn it on again.
+
+Given this: [2,5,2,1,1,4]
+
+Go to index 0 and flip whatever value index 0 pointing to[if it was not flipped(turned on) before]...
+
+What does that mean?
+
+Index 0 has value 2. Flip whatever 2 is pointing-to. Since the array is 0 based, so we will flip 2-1 = 1 and that is 5.
+
+So, our result will look like:[2,-5,2,1,1,4]
+*/
+
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-       vector<int>twiceNums;
+        vector<int>twiceNums;
 
-       sort(nums.begin(),nums.end()); // 
+        int n = nums.size();
 
-       // 1 2 3 2 3 => 1 2 2 3 3
+        for(int i=0;i<n;i++){
+            int ind = abs(nums[i])-1;
 
-       for(int i=0;i<nums.size()-1;i++){
-        if(nums[i]==nums[i+1]){
-            twiceNums.push_back(nums[i]);
+            if(nums[ind]<0){
+                twiceNums.push_back(abs(nums[i]));
+            }
+            else{
+                nums[ind]=-nums[ind];
+            }
         }
-       }
-
         return twiceNums;
     }
 };
