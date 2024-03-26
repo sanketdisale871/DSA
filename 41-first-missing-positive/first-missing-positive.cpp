38e4310e9f00eccebe77e14a3223e94b96af3415
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-03-26 07:15:27 +0530

    Time: 43 ms (62.28%) | Memory: 43.6 MB (81.44%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -3,17 +3,18 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
 
+        bool isOnePresent = false;
 
-        bool isOneFound = false;
-        for(auto it:nums){
-            if(it==1){
-                isOneFound = true;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                isOnePresent = true;
             }
         }
 
-        if(isOneFound){
 
-            // make all are inclusive [1,...n]
+        if(isOnePresent){
+            // Mark the 1 as out of range 
+
             for(int i=0;i<n;i++){
                 if(nums[i]<1 || nums[i]>n){
                     nums[i]=1;
@@ -22,12 +23,12 @@ public:
 
             for(int i=0;i<n;i++){
                 int ind = abs(nums[i])-1;
+
                 if(nums[ind]>0){
                     nums[ind]=-1*nums[ind];
                 }
             }
 
-            // Traverse and Find Out the which one is missing 
             for(int i=0;i<n;i++){
                 if(nums[i]>0){
                     return i+1;
@@ -39,6 +40,5 @@ public:
         else{
             return 1;
         }
-
     }
 };
