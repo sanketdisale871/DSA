5152c5318ae630f32d564b0a1999b966420afb25
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-04-01 07:57:41 +0530

    Time: 5 ms (47.87%) | Memory: 7.9 MB (7.54%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int len = 0; // Length of Last Word
+        
+        string prev = "";
+        string crr = "";
 
-        reverse(s.begin(),s.end());
-
-        int itr = 0;
-
-        while(s[itr]==' ' && itr<s.length()){
-            itr++;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==' ' && i>0 && s[i-1]!=' '){
+                prev = crr;
+                crr = "";
+            }
+            else if(s[i]==' '){
+                continue;
+            }
+            else{
+                crr.push_back(s[i]);
+            }
         }
 
-        while(s[itr]!=' ' && itr<s.length()){
-            len++;
-            itr++;
+        if(crr.length()>0){
+            return crr.length();
         }
 
-
-        return len;
+        return prev.length();
     }
 };
