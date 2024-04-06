2353bd0963a84f8cbd25624be36915997b2d1d8c
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-04-06 14:06:25 +0530

    Time: 9 ms (37.01%) | Memory: 12.5 MB (52.82%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    private:
+    vector<vector<int>>ans;
+    void solve(int ind,vector<int>&ar,int tar,vector<int>&tmp){
+
+        if(tar==0){
+            ans.push_back(tmp);
+            return ;
+        }
+        // base case 
+        if(ind>=ar.size()){
+            return;
+        }
+
+        // Pick 
+        if(ar[ind]<=tar){
+            tmp.push_back(ar[ind]);
+            solve(ind,ar,tar-ar[ind],tmp);
+            tmp.pop_back();
+        }
+
+        solve(ind+1,ar,tar,tmp);
+        // Non-Pick
+    }
+
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        vector<int>tmp;
+
+        solve(0,candidates,target,tmp);
+
+
+        return ans;
+    }
+};
