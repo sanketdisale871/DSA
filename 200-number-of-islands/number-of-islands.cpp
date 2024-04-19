802e5e6532f1fb4664a9950fbbd4f55d8b1a8df1
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-04-19 08:52:46 +0530

    Time: 30 ms (50.51%) | Memory: 16.8 MB (48.92%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,17 +1,19 @@
 class Solution {
     private:
-    void doDfsTrav(int i,int j,vector<vector<char>>& grid,vector<vector<int>>&vis){
-        vis[i][j]=1;
+    void dfs(int r,int c,vector<vector<char>>&grid,vector<vector<int>>&vis){
+        vis[r][c]=1;
 
         int drow[]={-1,1,0,0};
-        int dcol[]={0,0,-1,1};
+        int dcol[]={0,0,1,-1};
 
-        for(int ii=0;ii<4;ii++){
-            int newX = drow[ii]+i;
-            int newY = dcol[ii]+j;
+        for(int i=0;i<4;i++){
+            int newX = drow[i]+r;
+            int newY = dcol[i]+c;
 
-            if((newX>=0 && newX<grid.size()) && (newY>=0 && newY<grid[0].size()) && (!vis[newX][newY]) && grid[newX][newY]=='1'){
-                doDfsTrav(newX,newY,grid,vis);
+
+            if((newX>=0 && newX<grid.size()) && (newY>=0 && newY<grid[0].size()) && grid[newX][newY]=='1'
+            && !vis[newX][newY]){
+                dfs(newX,newY,grid,vis);
             }
         }
     }
@@ -22,17 +24,17 @@ public:
 
         vector<vector<int>>vis(m,vector<int>(n,0));
 
-        int ans = 0;
+        int cnt = 0;
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
-                    ans++;
-                    doDfsTrav(i,j,grid,vis);
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    dfs(i,j,grid,vis);
+                    cnt++;
                 }
             }
         }
 
-        return ans;
+        return cnt;
     }
 };
