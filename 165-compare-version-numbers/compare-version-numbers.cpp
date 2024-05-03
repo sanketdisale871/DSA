1855887a5dd0dbf1ccd0fa52407c61ae6789a79c
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-05-03 13:22:13 +0530

    Time: 3 ms (39.03%) | Memory: 7.3 MB (93.86%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -3,26 +3,20 @@ public:
     int compareVersion(string v1, string v2) {
         int i=0,j=0,n1=v1.length(),n2=v2.length();
 
-        string rev1="",rev2="";
-
+        int nu1=0,nu2=0;
         while(i<n1 && j<n2){
             // cout<<"i : "<<i<<" j: "<<j<<endl;
             while(v1[i]!='.' && i<n1){
-                rev1.push_back(v1[i]);
+                nu1 = (nu1*10)+(v1[i]-'0');
                 i++;
             }
             i++;
 
             while(v2[j]!='.' && j<n2){
-                rev2.push_back(v2[j]);
+               nu2 = (nu2*10)+(v2[j]-'0');
                 j++;
             }
             j++;
-
-            int nu1 = stoi(rev1);
-            rev1="";
-            int nu2 = stoi(rev2);
-            rev2="";
 
             if(nu1>nu2){
                 return 1;
@@ -30,6 +24,8 @@ public:
             else if(nu1<nu2){
                 return -1;
             }
+
+            nu1=0;nu2=0;
         }
 
         if(i>=n1 && j>=n2){
@@ -39,33 +35,32 @@ public:
         while(i<n1){ 
 
             while(i<n1 && v1[i]!='.'){
-                rev1.push_back(v1[i]);
+               nu1 = (nu1*10)+(v1[i]-'0');
                 i++;
             }
             i++;
-            int nu1 = stoi(rev1);
-            rev1="";
 
             if(nu1>0){
                 return 1;
             }
+
+            nu1=0;
         }
 
         while(j<n2){ 
 
             while(j<n2 && v2[j]!='.'){
-                rev2.push_back(v2[j]);
+                 nu2 = (nu2*10)+(v2[j]-'0');
                 j++;
             }
             j++;
-            int nu2 = stoi(rev2);
-            rev2="";
+           
 
             if(nu2>0){
                 return -1;
             }
+            nu2=0;
         }
-
         return 0;
 
 
