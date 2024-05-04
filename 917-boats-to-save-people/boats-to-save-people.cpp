b2426c772c443808ca600e41ff45274297e43da4
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-05-04 08:51:25 +0530

    Time: 64 ms (20.84%) | Memory: 45.4 MB (85.44%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
+// Intitution : Maximum ppl weight ke sath, minimum wala hi jugad dunga.
     int numRescueBoats(vector<int>& ppl, int limit) {
         sort(ppl.begin(),ppl.end());
         int n = ppl.size();
         int i=0,j=n-1;
         int rqBot = 0;
 
-        while(i<j){
-            if(ppl[j]>=limit){
-                rqBot++;
-                j--;
-            }
-            else{
-                if(ppl[i]+ppl[j]<=limit){
-                    rqBot++;
-                    i++;j--;
+        while(i<=j){
+
+            if(ppl[i]+ppl[j]<=limit){
+                    i++;
                 }
-                else{
-                    rqBot++;
-                    j--;
-                }
-            }
+
+            rqBot++;
+            j--;
         }
 
-        if(i==j){
-            rqBot++;
-        }
+      
+
         return rqBot;
     }
 };
