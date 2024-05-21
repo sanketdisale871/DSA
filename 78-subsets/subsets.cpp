b5f9f74d79ecf3add0be5427a77b890405268619
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-05-21 06:22:35 +0530

    Time: 2 ms (53.70%) | Memory: 8.3 MB (62.90%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,25 +1,20 @@
 class Solution {
-    public:
-    vector<vector<int>>ans;
-    void genAllSubsets(int ind,vector<int>&nums,vector<int>temp){
-        // base case 
-        if(ind>=nums.size()){
-            ans.push_back(temp);
-            return;
-        }
-
-        temp.push_back(nums[ind]);
-        genAllSubsets(ind+1,nums,temp);
-        temp.pop_back();
-
-        genAllSubsets(ind+1,nums,temp);
-    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>temp;
-        
-        genAllSubsets(0,nums,temp);
+        int sizOfArray = nums.size();
+        int totElems = (1<<sizOfArray); // 
 
+        vector<vector<int>>ans; // Total Subsets 
+
+        for(int mask = 0;mask<totElems;mask++){
+            vector<int>temp;
+            for(int itr=0;itr<sizOfArray;itr++){
+                if(mask & (1<<itr)){
+                    temp.push_back(nums[itr]);
+                }
+            }
+            ans.push_back(temp);
+        }
         return ans;
     }
 };
