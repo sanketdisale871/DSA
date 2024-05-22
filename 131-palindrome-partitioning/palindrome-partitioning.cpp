0d318aedd8e66113400a803d5e381838c17ded74
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-05-22 22:15:59 +0530

    Time: 190 ms (9.50%) | Memory: 174.8 MB (6.00%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -2,30 +2,29 @@ class Solution {
     private:
     vector<vector<string>>ans;
 
-    bool isPalindrome(int l,int r,string s){
-        while(l<r){
-            if(s[l]!=s[r]){
+    bool isPalindrome(int l,int h,string s){
+        while(l<=h){
+            if(s[l]!=s[h]){
                 return false;
             }
             l++;
-            r--;
+            h--;
         }
         return true;
     }
 
-    void solve(int ind,string s,vector<string>&temp){
-        // Base case 
-        if(ind>=s.length()){
+    void solve(int ind,string s,int n,vector<string>temp){
+        // base case 
+        if(ind>=n){
             ans.push_back(temp);
-            return;
+            return ;
         }
 
-        for(int i=ind;i<s.length();i++){
+        for(int i=ind;i<n;i++){
             if(isPalindrome(ind,i,s)){
-                temp.push_back(s.substr(ind,i-ind+1));
-
-                solve(i+1,s,temp);
-
+                string res = s.substr(ind,i-ind+1);
+                temp.push_back(res);
+                solve(i+1,s,n,temp);
                 temp.pop_back();
             }
         }
@@ -35,8 +34,8 @@ public:
     vector<vector<string>> partition(string s) {
         
         vector<string>temp;
-
-        solve(0,s,temp);
+        
+        solve(0,s,s.length(),temp);
 
         return ans;
     }
