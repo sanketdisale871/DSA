75800122146726107b6fb259cac17ddeacacfed7
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-08-04 10:03:25 +0530

    Time: 5 ms (57.94%) | Memory: 16.3 MB (33.71%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -4,50 +4,49 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        // pair<t,pair<r,c>>
-        queue<pair<int,pair<int,int>>>q;
+        // pair<node,dist>
+        queue<pair<pair<int,int>,int>>q;
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    q.push({0,{i,j}});
+                    q.push({{i,j},0});
                 }
             }
         }
 
-        int ans = 0;
+        int maxiTime = 0;
+
+        int drow[] ={-1,1,0,0};
+        int dcol[] ={0,0,1,-1};
 
         while(!q.empty()){
             auto it = q.front();q.pop();
+            int u = it.first.first;
+            int v = it.first.second;
+            int wt = it.second;
 
-            ans = max(ans,it.first);
-            int x = it.second.first;
-            int y = it.second.second;
-
-            int drow[] = {-1,1,0,0};
-            int dcol[] = {0,0,-1,1};
+            maxiTime = max(maxiTime,wt);
 
             for(int i=0;i<4;i++){
-                int newX = x+drow[i];
-                int newY = y+dcol[i];
+                int newX = drow[i]+u;
+                int newY = dcol[i]+v;
 
-                if((newX>=0 && newX<m) && (newY>=0 && newY<n) && 
-                grid[newX][newY]==1){
-                    grid[newX][newY]=-1;
-                    q.push({it.first+1,{newX,newY}});
+                if((newX>=0 && newX<m) && (newY>=0 && newY<n) && grid[newX][newY]==1){
+                    grid[newX][newY]=2;
+                    q.push({{newX,newY},wt+1});
                 }
             }
         }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-            
                 if(grid[i][j]==1){
                     return -1;
                 }
             }
         }
-
-        return ans;
+        
+        return maxiTime;
     }
 };
