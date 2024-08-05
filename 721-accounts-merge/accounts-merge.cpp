9e339385b6fcdb981dc6c5579bed3697ce08451a
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-08-05 15:29:50 +0530

    Time: 60 ms (85.25%) | Memory: 37.9 MB (64.12%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -1,94 +1,108 @@
 class DisjointSet{
     public:
-    vector<int>size,parent;
-    
+    vector<int>parent;
+    vector<int>size;
+
     DisjointSet(int n){
         parent.resize(n+1);
         size.resize(n+1,1);
-        
-        for(int i=0;i<n+1;i++){
+
+        for(int i=0;i<=n;i++){
             parent[i]=i;
         }
     }
-    
+
     int findUltPar(int node){
         if(node == parent[node]){
-            return node;
+            return parent[node];
         }
-        
+
         return parent[node]=findUltPar(parent[node]);
     }
-    
+
     void unionBySize(int u,int v){
-        int ultp_u = findUltPar(u);
-        int ultp_v = findUltPar(v);
-        
-        if(ultp_u==ultp_v){
-           return;
+        int ult_u = findUltPar(u);
+        int ult_v = findUltPar(v);
+
+        if(ult_u==ult_v){
+            return ;
         }
-        
-        if(size[ultp_u]<size[ultp_v]){
-            parent[ultp_u]=ultp_v;
-            size[ultp_v]+=size[ultp_u];
+
+        if(size[ult_u]<size[ult_v]){
+            size[ult_v]+=size[ult_u];
+            parent[ult_u]=ult_v;
         }
         else{
-            parent[ultp_v]=ultp_u;
-            size[ultp_u]+=size[ultp_v];
+            size[ult_u]+=size[ult_v];
+            parent[ult_v]=ult_u;
         }
     }
 };
 
 class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accnt) {
-        vector<vector<string>> ans;
-        int n = accnt.size();
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accn) {
+        
+        // cout<<"Ram Krishna Hari!!"<<endl;
+
+        /*
+        Problem Explanation : 
+        -> Name, Mail Id given
+        -> If mail id same, that person and that have in same mail id
+        -> Person => All mail IDs
+
+        -- How i am looking : 
+        -> Accordin gmail id indexing karata, 
+        -> unko bad mai merge karta.        
+        
+        */
+        int n = accn.size();
+
+        DisjointSet ds(n+1);
 
         unordered_map<string,int>um;
 
-        DisjointSet ds(n);
-
-        // Step1 : Indexing of Mails with their index 
-        for(int i=0;i<n;i++){
-            for(int j=1;j<accnt[i].size();j++){
-                if(um.find(accnt[i][j])==um.end()){
-                    um[accnt[i][j]]=i;
+        for(int i=0;i<accn.size();i++){
+            for(int j=1;j<accn[i].size();j++){
+                if(um.find(accn[i][j])!=um.end()){
+                    int v = um[accn[i][j]];
+                    ds.unionBySize(i,v);
                 }
                 else{
-                    ds.unionBySize(i,um[accnt[i][j]]);
+                    um[accn[i][j]]=i;
                 }
             }
         }
 
-        // Step 2 : Merging the account of going same parent
-        vector<string>merge[n];
+        vector<string>v[n];
 
         for(auto it:um){
-            string mail = it.first;
-            int node =  ds.findUltPar(it.second);
+            int u = ds.findUltPar(it.second);
 
-            merge[node].push_back(mail);
+            v[u].push_back(it.first);
         }
 
-        
+        vector<vector<string>>ans;
 
         for(int i=0;i<n;i++){
-            if(merge[i].size()==0){
+            if(v[i].empty()){
                 continue;
             }
 
-            sort(merge[i].begin(),merge[i].end());
+            sort(v[i].begin(),v[i].end());
 
             vector<string>temp;
+            temp.push_back(accn[i][0]);
 
-            temp.push_back(accnt[i][0]);
-
-            for(auto it:merge[i]){
+            for(auto it:v[i]){
                 temp.push_back(it);
             }
-
             ans.push_back(temp);
         }
         return ans;
+
+        
+
+        return {{}};
     }
 };
