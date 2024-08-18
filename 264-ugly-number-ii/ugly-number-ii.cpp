f9eb9a04c470be46ba05d9737c83ce36832be24a
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-08-18 22:23:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (56.48%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    typedef long long int ll;
+
     int nthUglyNumber(int n) {
-        set<ll>st;
-
-        ll nthNumer =1;
-
-        st.insert(nthNumer);
-
-        for(ll i=0;i<n;i++){
-            nthNumer = *st.begin();
-
-            st.erase(nthNumer);
-            st.insert(nthNumer*2);
-            st.insert(nthNumer*3);
-            st.insert(nthNumer*5);
+       vector<int> dp(n);
+        dp[0]=1;
+        int x=0, y=0, z=0;
+        for(int i=1; i<n; i++){
+            dp[i] = min(dp[x]*2, min(dp[y]*3, dp[z]*5));
+            if(dp[i] == 2*dp[x]) x++;
+            if(dp[i] == 3*dp[y]) y++;
+            if(dp[i] == 5*dp[z]) z++;
         }
-        return nthNumer;
+        return dp[n-1];
     }
 };
