c2ef7bbca14b3cdbaa90424287b74c933fbbb70e
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-09-29 14:23:16 +0530

    Time: 110 ms (23.35%) | Memory: 58.7 MB (59.42%) - LeetSync

--- 432-all-oone-data-structure/all-oone-data-structure.cpp
+++ 432-all-oone-data-structure/all-oone-data-structure.cpp
@@ -1,24 +1,8 @@
-#include <unordered_map>
-#include <unordered_set>
-#include <string>
-#include <functional>
-
-// Custom hash function for std::pair<int, std::string>
-// struct pair_hash {
-//     template <class T1, class T2>
-//     std::size_t operator()(const std::pair<T1, T2>& p) const {
-//         auto hash1 = std::hash<T1>{}(p.first);
-//         auto hash2 = std::hash<T2>{}(p.second);
-//         return hash1 ^ (hash2 << 1); // Combine the two hash values
-//     }
-// };
-
 class AllOne {
 public:
-    // Use the custom hash function for unordered_set
-    set<std::pair<int, std::string>> incrs;
-  set<std::pair<int, std::string>, std::greater<>> decrs;
-    std::unordered_map<std::string, int> um;
+    set<pair<int,string>> incrs;
+    set<pair<int,string>,greater<>> decrs;
+    unordered_map<string, int> um;
 
     AllOne() {
     }
@@ -45,7 +29,7 @@ public:
             incrs.insert({um[key], key});
             decrs.insert({um[key], key});
         } else {
-            um.erase(key);  // Remove the key from the map if the count becomes 0
+            um.erase(key);  
         }
     }
 
@@ -53,7 +37,6 @@ public:
         if (decrs.empty()) {
             return "";
         }
-
         return decrs.begin()->second;
     }
 
@@ -61,7 +44,6 @@ public:
         if (incrs.empty()) {
             return "";
         }
-
         return incrs.begin()->second;
     }
 };
