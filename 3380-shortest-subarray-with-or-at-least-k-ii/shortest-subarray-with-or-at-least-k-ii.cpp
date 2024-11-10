77250a948f45d7ff8d8a33381dba2cb8b026da7e
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-11-10 12:31:10 +0530

    Time: 675 ms (16.48%) | Memory: 355.5 MB (5.01%) - LeetSync

--- 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,72 +1,72 @@
 class Solution {
+    private:
     typedef long long int ll;
     
-    vector<vector<ll>>setBits;
-    
-    void preCal(vector<int>&nums){
-        ll n = nums.size();
-        
-        setBits.resize(n+1,vector<ll>(32,0));
-        
-        for(ll i=0;i<n;i++){
-            for(ll bit=0;bit<32;bit++){
-                
-                if(nums[i]&(1<<bit)){
-                    setBits[i+1][bit]=1+setBits[i][bit];
-                }
-                else{
-                    setBits[i+1][bit]=setBits[i][bit];
-                }
-            }
-        }        
-    }
-    
-    bool isPossible(vector<int>&nums,ll n,ll len,ll k){
-        // ll left = 1;
-        ll right = len;
-        
-        for(ll i=right;i<=n;i++){
-            
+    bool isPossible(ll len,vector<vector<ll>> &preSetBits,ll k){
+        ll n = preSetBits.size();
+
+        for(ll i=1;i<n-len;i++){
+            ll j = i+len;
+
             ll res = 0;
-            
+
             for(ll bit=0;bit<32;bit++){
-                ll setBitss = setBits[i][bit]-setBits[i-len][bit];
-                
-                if(setBitss>0){
-                    res = (res|(1<<bit));
-                }                
+                ll r = preSetBits[j][bit]-preSetBits[i-1][bit];
+
+                if(r>0){
+                    res = res | (1<<bit);
+                }
             }
-            
+
             if(res>=k){
                 return true;
             }
-        }        
+        }
         return false;
+        
     }
-    
+
 public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
-        ll ans = -1;
-        
-        preCal(nums);
-        
         ll n = nums.size();
-        ll low = 1;
+        vector<vector<ll>>preSetBits(n+1,vector<ll>(32,0));
+
+
+        for(ll i=0;i<n;i++){
+            for(ll bit=0;bit<32;bit++){
+                if(nums[i] & (1<<bit)){
+                    preSetBits[i+1][bit]=1+preSetBits[i][bit];
+                }
+                else{
+                    preSetBits[i+1][bit] = preSetBits[i][bit];
+                }
+// 
+                // cout<<" i : "<<i<<" bit : "<<bit<<" "<<preSetBits[i+1][bit]<<endl;
+            }
+        }
+
+        ll low = 0;
         ll high = n;
-        
+
+        ll ans = -1;
         while(low<=high){
             ll mid = low + (high-low)/2;
-            
-            if(isPossible(nums,n,mid,k)){
-                ans = mid;
-                high = mid-1;
+
+            ll r = isPossible(mid,preSetBits,k);
+
+            // cout<<" mid : "<<mid<<" r : "<<r<<endl;
+
+            if(r){
+                ans = mid+1;
+                high =mid-1;
             }
             else{
                 low = mid+1;
             }
+
+
         }
-       
-        
         return ans;
+
     }
 };
