7fa176e29bd043383f9466a918ba6bd35cc5e681
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-11-16 18:48:11 +0530

    Time: 0 ms (100.00%) | Memory: 33.2 MB (32.15%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -3,25 +3,29 @@ public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         vector<int>ans;
 
-        for(int i=0;i<=nums.size()-k;i++){
-            bool fl = true;
-            int maxElem = nums[i];
-            for(int j=i+1;j<i+k;j++){
-                if(nums[j-1]+1 != nums[j]){
-                    fl = false;
-                    break;
-                }
-                else{
-                    maxElem = max(maxElem,nums[j]);
-                }
-            }
-            if(fl){
-                ans.push_back(maxElem);
+        int cons = 1;
+
+        int n = nums.size();
+
+        for(int i=0;i<n;i++){
+            if( i>0 &&nums[i]==nums[i-1]+1){
+                cons++;
             }
             else{
-                ans.push_back(-1);
+                cons = 1;
+            }
+
+
+            if(i>=k-1){
+                if(cons>=k){
+                    ans.push_back(nums[i]);
+                }
+                else{
+                    ans.push_back(-1);
+                }
             }
         }
+
 
         return ans;
     }
