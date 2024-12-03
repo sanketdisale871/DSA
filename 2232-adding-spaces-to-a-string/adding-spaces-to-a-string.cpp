e8808d07be6329c7ddb3ba9be63739a63d94cb40
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-12-03 09:59:14 +0530

    Time: 29 ms (14.16%) | Memory: 85.5 MB (18.48%) - LeetSync

---
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& sp) {
+        string ans="";
+
+        int n = s.length();
+
+        sort(sp.begin(),sp.end());
+        
+        int n2=sp.size();
+        int j=0;
+
+        while(j<n2 && sp[j]==0){
+            ans.push_back(' ');
+            j++;
+        }
+
+        for(int i=0;i<n;i++){
+
+            if(j<n2){
+                if(i==sp[j]-1){
+                    ans.push_back(s[i]);
+                    ans.push_back(' ');
+                    j++;
+                }
+                else{
+                    ans.push_back(s[i]);
+                }
+            }
+            else{
+                ans.push_back(s[i]);
+            }
+        }
+
+        return ans;
+    }
+};
