b02ba58e9e929cd94a359b735a9ba5484daccd39
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2024-12-22 17:46:55 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (30.76%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -4,49 +4,52 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        // pair<node,dist>
-        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>vis(m,vector<int>(n,0));
+
+        // pair<cst,pair<int,int>>
+        queue<pair<int,pair<int,int>>>q;
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j},0});
+                    q.push({0,{i,j}});
+                    vis[i][j]=1;
                 }
             }
         }
 
-        int maxiTime = 0;
+        int drow[]={-1,1,0,0};
+        int dcol[]={0,0,-1,1};
 
-        int drow[] ={-1,1,0,0};
-        int dcol[] ={0,0,1,-1};
+        int t = 0;
 
         while(!q.empty()){
             auto it = q.front();q.pop();
-            int u = it.first.first;
-            int v = it.first.second;
-            int wt = it.second;
+            int cst = it.first;
+            int x = it.second.first;
+            int y = it.second.second;
 
-            maxiTime = max(maxiTime,wt);
+            t = max(t,cst);
 
             for(int i=0;i<4;i++){
-                int newX = drow[i]+u;
-                int newY = dcol[i]+v;
+                int newX = x + drow[i];
+                int newY = y + dcol[i];
 
-                if((newX>=0 && newX<m) && (newY>=0 && newY<n) && grid[newX][newY]==1){
-                    grid[newX][newY]=2;
-                    q.push({{newX,newY},wt+1});
+                if(newX>=0 && newX<m && newY>=0 && newY<n && grid[newX][newY]==1 && !vis[newX][newY]){
+                    vis[newX][newY]=1;
+                    q.push({cst+1,{newX,newY}});
                 }
-            }
+            }          
         }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
+                if(grid[i][j]==1 && vis[i][j]==0){
                     return -1;
                 }
             }
         }
-        
-        return maxiTime;
+        return t;
+
     }
 };
