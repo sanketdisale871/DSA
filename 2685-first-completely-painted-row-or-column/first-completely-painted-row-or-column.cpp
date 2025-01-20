f1a76f61dedd6349e161de8deb420c60eb88037f
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-01-20 21:37:44 +0530

    Time: 103 ms (48.78%) | Memory: 161.4 MB (77.74%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        // I will calculate the how many each row and column will get
-        // maximum time to paint the row/col and taking minimum from them
-
-        int m = mat.size();
-        int n = mat[0].size();
-
-        int ans = INT_MAX;
-
-        // Hash the arr with index
         unordered_map<int,int>um;
+
         for(int i=0;i<arr.size();i++){
             um[arr[i]]=i;
         }
 
-        // traverse the matrix for row How many taking time, taking minimum from them
-        for(int i=0;i<m;i++){
-            int rowMax = 0;
-            for(int j=0;j<n;j++){
-                rowMax = max(rowMax,um[mat[i][j]]);
+        int smInd = INT_MAX;
+
+        for(int i=0;i<mat.size();i++){
+            int maxRat = 0;
+            for(int j=0;j<mat[0].size();j++){
+                maxRat = max(maxRat,um[mat[i][j]]);
             }
-            ans = min(ans,rowMax);
+            smInd = min(smInd,maxRat);
         }
 
-        // Traverse the matrix for column, How many taking time,taking minimum from them
-
-        for(int j=0;j<n;j++){
-            int colMax = 0;
-            for(int i=0;i<m;i++){
-                colMax = max(colMax,um[mat[i][j]]);
+        for(int j=0;j<mat[0].size();j++){
+            int maxRat = 0;
+            for(int i=0;i<mat.size();i++){
+                maxRat = max(maxRat,um[mat[i][j]]);
             }
-            ans = min(ans,colMax);
+            smInd = min(smInd,maxRat);
         }
-
-        return ans;       
+        return smInd;
     }
 };
