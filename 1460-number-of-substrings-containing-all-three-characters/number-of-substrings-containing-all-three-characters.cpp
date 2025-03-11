78934c29d6e65454c431f25b299fc3f8248b5053
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-03-11 15:31:36 +0530

    Time: 22 ms (31.09%) | Memory: 11.1 MB (57.30%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -3,32 +3,42 @@ public:
     int numberOfSubstrings(string s) {
         int n = s.length();
 
+        int i=0,j=0;
+
+        unordered_map<char,int>um = {
+            {'a',1},{'b',1},{'c',1}
+        };
+        int cnt = 3;
         int ans = 0;
 
-        int i=0,j=0;
-        unordered_map<char,int>um;
 
         while(j<n){
-            um[s[j]]++;
+            um[s[j]]--;
 
-            if(um.size()<3){
+            if(um[s[j]]==0){
+                cnt--;
+            }
+
+            if(cnt>0){
                 j++;
             }
             else{
-                while(um.size()>=3 && i<=j){
-                    ans+=(n-j);
 
-                    um[s[i]]--;
+                    while(i<=j && cnt<=0){
+                        ans+=(n-j);
 
-                    if(um[s[i]]==0){
-                        um.erase(s[i]);
+                    um[s[i]]++;
+
+                    if(um[s[i]]==1){
+                        cnt++;
                     }
                     i++;
-                }
+                    }
+                    
+                
                 j++;
             }
         }
-
         return ans;
     }
 };
