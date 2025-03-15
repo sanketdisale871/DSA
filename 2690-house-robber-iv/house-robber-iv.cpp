f4204e00842eefd2d36c42d72d0b69d37a80ca10
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-03-15 22:17:44 +0530

    Time: 26 ms (51.30%) | Memory: 60.8 MB (67.69%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,36 +1,36 @@
 class Solution {
     private:
-    bool isPossible(vector<int>&nums,int maxAmnt,int k){
+    bool isPossible(int mid,vector<int>&nums,int k){
+        int houseRobbed = 0;  
         int i=0;
-        int n = nums.size();
-        int house = 0;
 
-        while(i<n){
-            if(nums[i]<=maxAmnt){
-                house++;
+        while(i<nums.size()){
+            if(nums[i]<=mid){
+                houseRobbed++;
                 i+=2;
             }
             else{
                 i++;
             }
         }
-        return house>=k;
+        return houseRobbed>=k;
     }
 public:
     int minCapability(vector<int>& nums, int k) {
         int low = *min_element(nums.begin(),nums.end());
         int high = *max_element(nums.begin(),nums.end());
-        int ans = 0;
-        
-        while(low<=high){
-            int mid = low+(high-low)/2;
 
-            if(isPossible(nums,mid,k)){
+        int ans=-1;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(isPossible(mid,nums,k)){
                 ans = mid;
                 high = mid-1;
             }
             else{
-                low =mid+1;
+                low = mid+1;
             }
         }
         return ans;
