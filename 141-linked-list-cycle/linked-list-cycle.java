b49efab432377ae0d625bb2b37067a6a63273600
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-07-21 11:12:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (61.66%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // Hashm 
+        /// If i got to know the double entry in that case 
+        // Slow & Fast pointer se I can 
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
