b552df19cded8d92ac710630fb0e7c45e8c53570
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-08-25 10:03:07 +0530

    Time: 29 ms (44.58%) | Memory: 17.5 MB (48.21%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,47 +1,44 @@
 class Solution {
     private:
-    bool checkIsItValid(int currRow,int currCol,int rows,int cols){
-        return currRow>=0&&currRow<rows&&currCol>=0&&currCol<cols;
+    bool isSafe(int newX,int newY,int rows,int cols){
+        return newX>=0 && newY>=0 && newX<rows && newY<cols;
     }
-    void visAdjacentCells(int currRow,int currCol,vector<vector<char>>& grid,vector<vector<int>>&notVisCells){
-        notVisCells[currRow][currCol]=1;
+
+    void dfsTraversal(int r,int c,vector<vector<char>>&grid,vector<vector<int>>&vis){
+        vis[r][c]=1;
 
         int drow[]={-1,1,0,0};
         int dcol[]={0,0,1,-1};
 
-        for(int itr=0;itr<4;itr++){
-            int dCurrRow = currRow+drow[itr];
-            int dCurrCol = currCol + dcol[itr];
+        for(int dr=0;dr<4;dr++){
+            int newX = drow[dr]+r;
+            int newY = dcol[dr]+c;
 
-            if(checkIsItValid(dCurrRow,dCurrCol,grid.size(),grid[0].size()) && grid[dCurrRow][dCurrCol]=='1' && !notVisCells[dCurrRow][dCurrCol]){
-                visAdjacentCells(dCurrRow,dCurrCol,grid,notVisCells);
+            if(isSafe(newX,newY,grid.size(),grid[0].size()) && grid[newX][newY]=='1' && !vis[newX][newY]){
+                dfsTraversal(newX,newY,grid,vis);
             }
         }
     }
 public:
-/*
-Approach: 
-1) Vis 
-2) No.of Islands = No.of Calls to visite sourrounding cells;
-*/
     int numIslands(vector<vector<char>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
 
-        vector<vector<int>>notVisCells(rows,vector<int>(cols,0));
+        vector<vector<int>>vis(rows,vector<int>(cols,0));
 
-        int noOfIslands=0;
 
-        for(int rowItr=0;rowItr<rows;rowItr++){
-            for(int colItr=0;colItr<cols;colItr++){
-                // Current cell is Land, and not visited
-                if(grid[rowItr][colItr]=='1' && !notVisCells[rowItr][colItr]){
-                    noOfIslands++;
-                    visAdjacentCells(rowItr,colItr,grid,notVisCells); // Traverse the adjacent lands which can contribute in land.
+        // Connecting adjacent cells horizontally or vertically. 
+        int islands = 0;
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    islands++;
+                    dfsTraversal(i,j,grid,vis);
                 }
             }
         }
 
-        return noOfIslands;
+        return islands;
     }
 };
