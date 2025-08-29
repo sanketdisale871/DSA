abba9d9a61f914ac9084e5c21e60ecb2d5e46ab3
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-08-29 13:00:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (86.22%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,13 +1,23 @@
 class Solution {
+    /*
+    No.Of. possible pairs (x,y) I have to calculate bahii: 
+    Alice=> Has to win the game 
+    First Lane= [1,n] => x
+    Second lane= [1,m] => y
+
+    Return No.of Possible pairs (x,y) that satisfy the condition.
+    */
 public:
-    typedef long long int ll;
+typedef long long int ll;
     long long flowerGame(int n, int m) {
-       ll en = m / 2, od = m - en, ans = 0;
+  
+        ll en = m / 2, od = m - en, ans = 0;
         
         for(int i = 1; i <= n; i++){
             if(i % 2)ans += en;
             else ans += od;
         }
         return ans;
+
     }
 };
