9ecdfac0fd4d017dce547f1579b51b8ebd27c27b
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-08-30 13:49:56 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (86.89%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,4 +1,10 @@
 class Solution {
+    /*
+  From Tier4 se I can go to the Tier 1 companies, toh, aur kya chahiye. hahahaha
+
+  Don't loose Swanket hopes, You need only one chance. Prepare aise karunga na, ki ek hi chance mai
+  nikal jayega.   
+    */
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
     vector<int> row(9, 0);
@@ -6,15 +12,17 @@ public:
     vector<int> boxes(9, 0);
     for (int i = 0; i < 9; i++)
         for (int j = 0; j < 9; j++) {
-            if (board[i][j] != '.') 
+            if (board[i][j] != '.') // board[i][j]!='.'
             {
+                //  board[i][j] = 5-'0' => 5,, 1<<5 => 32
                 int bit = 1 << (board[i][j] - '0');
-                int index = i/3 * 3 + j / 3 ;
+                //5th BIT i settled
+                int index = i/3 * 3 + j / 3 ; // 
                 if(row[i]&bit || col[j]&bit || boxes[index]&bit)
                     return false;
-                row[i] |= bit ;
-                col[j] |= bit ;
-                boxes[index] |= bit ;
+                row[i] |= bit ; // row[i]|=bit
+                col[j] |= bit ; // col[j]|=bit
+                boxes[index] |= bit ; // boxes[index]|=bit. boxes of index i setteled.
          }
      }
      return true;
