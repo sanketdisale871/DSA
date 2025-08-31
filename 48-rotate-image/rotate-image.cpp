3f1e582a8c49a5caafa82b080a1d5a3586df3887
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-08-31 16:26:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (68.72%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& mat) {
-        // Transpose taking 
         int m = mat.size();
         int n = mat[0].size();
 
+        // swap(mat[r][c],mat[c][r]);
+        // Transpose of Matrix 
+
         for(int i=0;i<m;i++){
             for(int j=0;j<=i;j++){
-                swap(mat[i][j],mat[j][i]);
+                swap(mat[i][j],mat[j][i]); // Yes, ye Transpose find kar dega
             }
         }
 
-        for(int i=0;i<m;i++){
-            reverse(mat[i].begin(),mat[i].end());
+        for(int r=0;r<m;r++){
+            reverse(mat[r].begin(),mat[r].end());
         }
-        
     }
 };
