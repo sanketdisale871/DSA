bbce731d6cc2a3f98ed19c69f03ebbab6a3d37d7
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-09-01 11:03:18 +0530

    Time: 1009 ms (8.19%) | Memory: 97.9 MB (66.37%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -1,21 +1,21 @@
 class Solution {
-    private:
-    double findChange(int pass, int stu){
-        double chng = double(pass+1/stu+1) - double(pass/stu);
-        return chng;
-    }
-
 public:
   double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
     auto profit = [&](double pass, double total) {
         return (pass + 1) / (total + 1) - pass / total;
     };
+
     double total = 0;
+    // priority_queue<pair<double,array<int,2>>>pq; // MaxHeap that I have, 
     priority_queue<pair<double, array<int, 2>>> pq;
+
     for (auto &c : classes) {
         total += (double) c[0] / c[1];
+        int t = profit(c[0], c[1]);
+        cout<<"t : "<<t<<endl;
         pq.push({profit(c[0], c[1]), {c[0], c[1]}});
     }
+
     while (extraStudents--) {
         auto [added_profit, c] = pq.top(); pq.pop();
         total += added_profit;
