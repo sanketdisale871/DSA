231ee0321fb31bc1788d13a845407b3ec14e93f4
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-09-20 07:02:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (60.19%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-
-        unordered_map<int,int>um;
+        // Just next greater elment I have to find re
         stack<int>st;
+        int n = nums2.size();
+        vector<int>ans;
         st.push(-1);
 
-        int secArraySize = nums2.size();
-
-        for(int i=secArraySize-1;i>=0;i--){
+        // Here I will map the 
+                  // Num, Greater 
+        unordered_map<int,int>um;
+        
+        for(int i=n-1;i>=0;i--){
             while(st.top()!=-1 && st.top()<nums2[i]){
                 st.pop();
             }
             um[nums2[i]]=st.top();
+
             st.push(nums2[i]);
         }
-        /*
-        nums2 = [1,3,4,2]               n = 4
-                 0 1 2 3 
-                 3 4 -1 -1              Stack Wali chiz hai bhaii: [3,4,-1]
-        */
 
         for(int i=0;i<nums1.size();i++){
             ans.push_back(um[nums1[i]]);
         }
-
         return ans;
     }
 };
