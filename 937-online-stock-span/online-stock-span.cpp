dd7837cbd3ce276a911f04b44607d253735db097
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-09-21 09:11:18 +0530

    Time: 23 ms (86.26%) | Memory: 92 MB (89.38%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,11 +1,16 @@
+/* Online Stock Span Problem*/
+// I have to return the span of Stock bhaiii
+
 class StockSpanner {
 public:
-    stack<pair<int,int>>st;
+// stack<pair<num,cnt>>
+    stack<pair<int,int>>st; // I need to have the solution
     StockSpanner() {
         
     }
     
     int next(int price) {
+        // Here I have to return the just ,, like this I have the 4 as the price. 
         int x = 1;
         while(!st.empty() && st.top().first<=price){
             x+=st.top().second;
