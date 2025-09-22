185848dc1f1decb7630b46e9bb7836ddfddb0a71
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-09-22 17:05:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (67.83%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        // How to find it i know bhaii Next Permutation 
+        int n = nums.size();
+
+        // I know ki how to find re
+        next_permutation(nums.begin(),nums.end());
+    }
+};
