1d25e0d087e0ef45def635b7106344fac953e114
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2025-10-01 17:17:35 +0530

    Time: 47 ms (50.60%) | Memory: 128.9 MB (50.30%) - LeetSync

--- 1286-constrained-subsequence-sum/constrained-subsequence-sum.cpp
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    typedef int ll;
+    typedef long long int ll;
     int constrainedSubsetSum(vector<int>& nums, int k) {
-        deque<ll>dq;
+        deque<ll>dq; // will keep track the maximum Sum that i got till 
+        int n = nums.size();
 
-        vector<ll>cpy = nums;
-        ll maxi =INT_MIN;
+        vector<int>cpy = nums;
+        int maxiSum = INT_MIN;
 
-        ll n = nums.size();
-
-        for(ll i=0;i<n;i++){
-            ll fntElem = dq.empty()?0:cpy[dq.front()];
-
+        for(int i=0;i<n;i++){
+            int fntElem = dq.empty()?0:cpy[dq.front()];
             cpy[i]=max(cpy[i],cpy[i]+fntElem);
-
-            maxi = max(maxi,cpy[i]);
+            maxiSum = max(maxiSum,cpy[i]);
 
             while(!dq.empty() && cpy[dq.back()]<cpy[i]){
                 dq.pop_back();
@@ -25,8 +22,40 @@ public:
                 dq.pop_front();
             }
         }
-        
-        return maxi;
 
+        return maxiSum;
     }
 };
+// ````
+// class Solution {
+// public:
+//     typedef int ll;
+//     int constrainedSubsetSum(vector<int>& nums, int k) {
+//         deque<ll>dq;
+
+//         vector<ll>cpy = nums;
+//         ll maxi =INT_MIN;
+
+//         ll n = nums.size();
+
+//         for(ll i=0;i<n;i++){
+//             ll fntElem = dq.empty()?0:cpy[dq.front()];
+
+//             cpy[i]=max(cpy[i],cpy[i]+fntElem);
+
+//             maxi = max(maxi,cpy[i]);
+
+//             while(!dq.empty() && cpy[dq.back()]<cpy[i]){
+//                 dq.pop_back();
+//             }
+//             dq.push_back(i);
+
+//             if(i>=k && dq.front()==i-k){
+//                 dq.pop_front();
+//             }
+//         }
+        
+//         return maxi;
+
+//     }
+// };
