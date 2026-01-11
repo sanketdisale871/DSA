1c240b0b25392d0fcfd0373aafaf97af0be2033f
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2026-01-11 06:56:12 +0000

    Time: 3 ms (96.51%) | Memory: 58.2 MB (43.81%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        int n = heights.size();
+    int furthestBuilding(vector<int>& hts, int bricks, int ladders) {
+        int n = hts.size();
         int totalBricksUsed = 0;
-        priority_queue<int> bricksUsed;
-        int reach = 0;
-        for (; reach < n-1; reach++) {
-            // If current height is higher, we don't have to use
-            // bricks or ladder.
-            if (heights[reach] >= heights[reach+1]) {
+        priority_queue<int>diffBrickUsed;
+
+        int reach=0;
+        for(;reach<hts.size()-1;reach++){
+            if(hts[reach]>=hts[reach+1]){
                 continue;
             }
-        
-            // We have to use either brick or ladder.
-            int diff = heights[reach+1] - heights[reach];
-        
-            if (totalBricksUsed + diff <= bricks) {
-                // Prefer bricks because we can later change from 
-                // bricks to ladder as we will see below.
-                totalBricksUsed += diff;
-                bricksUsed.push(diff);
-            } else if (ladders > 0) {
-                // If even bricks are insufficient, check ladders.
-                if (!bricksUsed.empty() && bricksUsed.top() > diff) {   // Alert: Corner case ** 
-                    totalBricksUsed = totalBricksUsed - bricksUsed.top() + diff;
-                    bricksUsed.pop();
-                    bricksUsed.push(diff); // Thanks to @amanda_86
+
+            int diff = hts[reach+1]-hts[reach];
+
+            if(diff+totalBricksUsed<=bricks){
+                totalBricksUsed+=diff;
+                diffBrickUsed.push(diff);
+            }
+            else if(ladders>0){
+                if(!diffBrickUsed.empty() && diffBrickUsed.top()>diff){
+                    totalBricksUsed = totalBricksUsed-diffBrickUsed.top()+diff;
+                    diffBrickUsed.pop();
+                    diffBrickUsed.push(diff);
                 }
                 ladders--;
-            } else {
-                // Unfortunately, it's not possible to go ahead any further.
+            } 
+            else{
                 break;
             }
         }
-        return reach; // This is the max we can go ahead.
+        return reach;
     }
 };
