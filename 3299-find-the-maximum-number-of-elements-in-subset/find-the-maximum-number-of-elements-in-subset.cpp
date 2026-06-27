ba8db635ff07cd34b5c7a851e9d99f3b24c4640e
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2026-06-27 16:42:49 +0530

    Time: 101 ms (55.43%) | Memory: 129.2 MB (14.86%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.cpp
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     typedef long long int ll;
     int maximumLength(vector<int>& nums) {
-        ll ans = 1;
-
         unordered_map<ll,ll>um;
-        for(auto num:nums){
-            um[num]++;
+
+        ll ans = 1;
+        // Whatever the frequency,here I'm attaching it.
+        for(auto it:nums){
+            um[it]++;
         }
 
-        // [1,1,1,1,1,1,1,1,2,4,8,16,32] => 7 (Length)
-       if(um.count(1)){
-           if(um[1]%2==0){
-               ans = um[1]-1;
-           }
-           else{
-               ans = um[1];
-           }
-       }
+        if(um.count(1)){
+            if(um[1]%2==0){
+                ans = um[1]-1;
+            }
+            else{
+                ans = um[1];
+            }
+        }
 
-        for(auto num : nums){
-            ll cnt = 0;
+        for(auto num:nums){
             ll val = num;
-            while(val<=1e9 && um.count(val) &&um[val]>=2 && val!=1){
+            ll cnt = 0;
+
+            while(val<=1e9 && um[val]>=2 && val!=1){
                 cnt+=2;
-                val*=val;
+                val = val*val;
             }
 
             if(um[val]==1){
@@ -35,6 +36,6 @@ public:
             }
             ans = max(ans,cnt);
         }
-        return ans;
+        return (int)ans;
     }
 };
